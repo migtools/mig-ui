5e64d9ddc574d7924ab23ba25a584913b193ac88
Erik Nelson <erik@nsk.io>
2019-02-08 12:05:12 -0500

    Add initial buildable webpack example

---
+++ webpack/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const buildMode = !!process.env['PRODUCTION'] ? 'production' : 'development';
+const outputFileName = `mig-ui.${buildMode}.bundle.js`
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: buildMode,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: outputFileName,
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js']
+  }
+};
