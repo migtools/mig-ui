6fb221c6c87c1a692e2ddb40337c15932c0e49ef
Erik Nelson <erik@nsk.io>
2019-02-13 18:56:10 -0500

    Various updates - working patternfly
    * bring in jest & vanilla patternfly
    * webpack support for pf

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
+import "patternfly/dist/img/logo-alt.svg"
+import "patternfly/dist/img/brand-alt.svg"
 
-import '@patternfly/react-core/dist/styles/base.css';
-
-export interface HelloProps {
-  compiler: string;
-  framework: string;
-}
-
-export const App = (props: HelloProps) => {
+export const App = () => {
   return (
-    <h1>Hello from {props.compiler} and {props.framework}!</h1>
+    <div id="wrapper" className="layout-pf layout-pf-fixed">
+      <div className="navbar-header">
+        <nav className="navbar navbar-pf-vertical">
+          <button type="button" className="navbar-toggle">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a href="/" className="navbar-brand">
+            <img className="navbar-brand-icon" src="/img/logo-alt.svg" alt="" /><img className="navbar-brand-name" src="/img/brand-alt.svg" alt="PatternFly Enterprise Application" />
+          </a>
+        </nav>
+      </div>
+    </div>
   )
 };
