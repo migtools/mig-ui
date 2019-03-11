5971ea8f81c094e7b6259e7d3588d77a56ba545b
Bohdan Iakymets <biakymet@redhat.com>
2019-03-11 20:50:26 +0100

    Added connected-react-router

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,2 +1,2 @@
-export * from './history';
+export { default as history } from './history';
 export * from './store';
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,8 +1,12 @@
-import { authentication } from "./authentication.reducer";
-import { alert } from "./alert.reducer";
-import { createStore, combineReducers } from "redux";
+import { authentication } from './authentication.reducer';
+import { alert } from './alert.reducer';
+import { connectRouter } from 'connected-react-router';
+import { history } from '../helpers';
+
+import { createStore, combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
+  router: connectRouter(history),
   authentication,
   alert
 });
