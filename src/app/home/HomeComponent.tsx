81536fdf504a1f318d850a64d24840ba314a8fb7
Andrei Veselov <andyvesel@ya.ru>
2019-04-01 19:30:34 +0200

    Add responsive design (#36)
    * Add react-styled-system lib
    
    * Add viewport element to index.local
    
    * Add flexWrap
    
    * Add StyledBox to Card

--- src/app/home/HomeComponent.tsx
+++ src/app/home/HomeComponent.tsx
@@ -205,7 +205,7 @@ class HomeComponent extends React.Component<IProps, IState> {
         <Page header={Header} sidebar={Sidebar}>
           <PageSection>
             <TextContent>
-              <Flex justifyContent="center">
+              <Flex justifyContent="center" flexWrap="wrap">
                 <CardComponent
                   title="Clusters"
                   dataList={this.props.clusterList}
