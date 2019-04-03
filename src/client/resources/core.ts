b0210ca4d19ae418de4fe909ce07e7e056e0c65e
Erik Nelson <ernelson@redhat.com>
2019-04-03 14:44:36 -0400

    Implement the full cluster create/fetch (#54)

---
+++ src/client/resources/core.ts
@@ -0,0 +1,68 @@
+import {
+  NamespacedResource,
+  ClusterResource,
+  IGroupVersionKindPlural,
+} from './common';
+
+export class CoreNamespacedResource extends NamespacedResource {
+  private _gvk: IGroupVersionKindPlural;
+  constructor(kind: CoreNamespacedResourceKind, namespace: string) {
+    super(namespace);
+
+    this._gvk = {
+      group: '',
+      version: 'v1',
+      kindPlural: kind,
+    };
+  }
+  gvk(): IGroupVersionKindPlural {
+    return this._gvk;
+  }
+
+  public listPath(): string {
+    // The core resources live at a unique api path for legacy reasons, and do
+    // not have an API group
+    return [
+      '/api',
+      this.gvk().version,
+      'namespaces',
+      this.namespace,
+      this.gvk().kindPlural,
+    ].join('/');
+  }
+}
+
+export class CoreClusterResource extends ClusterResource {
+  private _gvk: IGroupVersionKindPlural;
+  constructor(kind: CoreClusterResourceKind, namespace: string) {
+    super();
+
+    this._gvk = {
+      group: '',
+      version: 'v1',
+      kindPlural: kind,
+    };
+  }
+  gvk(): IGroupVersionKindPlural {
+    return this._gvk;
+  }
+
+  public listPath(): string {
+    // The core resources live at a unique api path for legacy reasons, and do
+    // not have an API group
+    return [
+      '/api',
+      this.gvk().group,
+      this.gvk().kindPlural,
+    ].join('/');
+  }
+}
+
+export enum CoreNamespacedResourceKind {
+  Pod = 'pods',
+  Secret = 'secrets',
+}
+
+export enum CoreClusterResourceKind {
+  Namespace = 'namespaces',
+}
