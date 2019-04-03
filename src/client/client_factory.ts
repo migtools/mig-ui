943390ee9f0d66903d00a21eda2a94cc67e5990b
Erik Nelson <erik@nsk.io>
2019-04-03 15:46:54 -0400

    Implement ClientFactory.forCluster

--- src/client/client_factory.ts
+++ src/client/client_factory.ts
@@ -33,27 +33,25 @@ export const ClientFactory = {
     return new ClusterClient(state.migMeta.clusterApi, state.auth.user.access_token);
   },
   forCluster: (clusterName: string, state: any) => {
-    const clusters = state.kube.clusterregistry_k8s_io.clusters;
-    const clusterNotFound = !(clusterName in clusters);
-    if (clusterNotFound) {
-      throw new ClientFactoryUnknownClusterError(clusterName);
-    }
-    const cluster = clusters[clusterName];
-    // TODO: Need to get some more information from the cluster registry about:
-    // 1) Exactly where should this baseURL be retrieved from the cluster registry object?
-    // Just guessing this path based on the published CRD...
-    // Any reason to prefer an endpoint, since it's a list?
-    // Will need an error if none of them exist?
-    // See: https://github.com/kubernetes/cluster-registry/blob/master/cluster-registry-crd.yaml
-
-    // const apiRoot = cluster.spec.kubernetesApiEndpoints.serverEndpoints[0].serverAddress;
-
-    // TODO: Where in the state tree is the token available? It's not going to
-    // be on the cluster, so it's going to require an additional call to the secret endpoint
-    // to get the actual secret contents.
-
-    // const token = state.kube.core.secrets.relevant_cluster.token;
-    // return new ClusterClient(apiRoot, cluster.token);
-    throw new Error('forCluster NOT IMPLEMENTED');
+    const { clusterApi, accessToken } = getAuthForCluster(clusterName, state);
+    return new ClusterClient(clusterApi, accessToken);
   },
 };
+
+interface IAuthDetails {
+  clusterApi: string;
+  accessToken: string;
+}
+
+function getAuthForCluster(clusterName: string, state: any): IAuthDetails {
+  const cluster = state.cluster.clusterList
+    .find(c => c.MigCluster.metadata.name === clusterName);
+  if (!cluster) {
+    throw new ClientFactoryUnknownClusterError(clusterName);
+  }
+  const clusterApi =
+    cluster.Cluster.spec.kubernetesApiEndpoints.serverEndpoints[0].serverAddress;
+  const accessToken = atob(cluster.Secret.data.token);
+
+  return { clusterApi, accessToken };
+}
