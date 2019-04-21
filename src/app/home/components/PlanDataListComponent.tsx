c651b65d09d63b03bf7ef1ec24b391d9110320b3
Erik Nelson <erik@nsk.io>
2019-04-21 10:05:14 -0400

    Remove type from other items as well

--- src/app/home/components/PlanDataListComponent.tsx
+++ src/app/home/components/PlanDataListComponent.tsx
@@ -11,7 +11,7 @@ import ClusterStatusIcon from './ClusterStatusIcon';
 import { LinkIcon } from '@patternfly/react-icons';
 import EmptyStateComponent from './EmptyStateComponent';
 
-const DataListComponent = ({ dataList, type, ...props }) => {
+const DataListComponent = ({ dataList, ...props }) => {
   if (dataList) {
     return (
       <React.Fragment>
@@ -28,22 +28,7 @@ const DataListComponent = ({ dataList, type, ...props }) => {
                   <span id="simple-item1">{listItem.metadata.name}</span>
                 </DataListCell>
                 <DataListCell width={2}>
-                  {type === 'cluster' && (
-                    <a
-                      target="_blank"
-                      href={`http://localhost:9999/${listItem.metadata.name}`}
-                    >
-                      {`http://localhost:9999/${listItem.metadata.name}`}
-                    </a>
-                  )}
-                  {type === 'storage' && (
-                    <a
-                      target="_blank"
-                      href={`http://localhost:9999/${listItem.metadata.name}`}
-                    >
-                      {`http://localhost:9999/${listItem.metadata.name}`}
-                    </a>
-                  )}
+                  TODO: Plan list item
                 </DataListCell>
                 <DataListCell width={2}>
                   <LinkIcon /> 0 associated migration plans
@@ -56,7 +41,7 @@ const DataListComponent = ({ dataList, type, ...props }) => {
                     <Box mx={1}>
                       <Button
                         onClick={() =>
-                          props.onRemoveItem(type, dataList[index].id)
+                          props.onRemoveItem('plan', dataList[index].id)
                         }
                         variant="danger"
                       >
@@ -72,7 +57,7 @@ const DataListComponent = ({ dataList, type, ...props }) => {
             <Flex alignItems="center" justifyContent="center">
               <Box>
 
-              <EmptyStateComponent type={type} />
+              <EmptyStateComponent type='plan' />
               </Box>
             </Flex>
           )}
