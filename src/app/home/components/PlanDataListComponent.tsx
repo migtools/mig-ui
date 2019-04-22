491f8e1885312aeed7f7e12dc9a2e34199f468c2
Ian Bolton <ibolton336@gmail.com>
2019-04-22 09:57:09 -0400

    wip migrate trigger (#105)

--- src/app/home/components/PlanDataListComponent.tsx
+++ src/app/home/components/PlanDataListComponent.tsx
@@ -9,100 +9,107 @@ import { Flex, Box } from '@rebass/emotion';
 import theme from '../../../theme';
 import ClusterStatusIcon from './ClusterStatusIcon';
 import EmptyStateComponent from './EmptyStateComponent';
-
+import MigrateModal from "../../plan/components/MigrateModal";
 const DataListComponent = ({ dataList, ...props }) => {
   if (dataList) {
     return (
       <React.Fragment>
         {dataList.length > 0 ? (
           <DataList aria-label="Simple data list example">
-              <DataListItem
-                key={0}
-                isExpanded={false}
-                aria-labelledby="simple-item1"
-              >
-                <DataListCell width={1}>
-                  <span>Name</span>
-                </DataListCell>
-                <DataListCell width={1}>
-                  <span>Migrations</span>
-                </DataListCell>
-                <DataListCell width={1}>
-                  <span>Source</span>
-                </DataListCell>
-                <DataListCell width={1}>
-                  <span>Target</span>
-                </DataListCell>
-                <DataListCell width={1}>
-                  <span>Repository</span>
-                </DataListCell>
-                <DataListCell width={1}>
-                  <span>Persistent Volumes</span>
-                </DataListCell>
-                <DataListCell width={3}>
-                  <span>Last Status</span>
-                </DataListCell>
-              </DataListItem>
+            <DataListItem
+              key={0}
+              isExpanded={false}
+              aria-labelledby="simple-item1"
+            >
+              <DataListCell width={1}>
+                <span>Name</span>
+              </DataListCell>
+              <DataListCell width={1}>
+                <span>Migrations</span>
+              </DataListCell>
+              <DataListCell width={1}>
+                <span>Source</span>
+              </DataListCell>
+              <DataListCell width={1}>
+                <span>Target</span>
+              </DataListCell>
+              <DataListCell width={1}>
+                <span>Repository</span>
+              </DataListCell>
+              <DataListCell width={1}>
+                <span>Persistent Volumes</span>
+              </DataListCell>
+              <DataListCell width={3}>
+                <span>Last Status</span>
+              </DataListCell>
+            </DataListItem>
             {dataList.map((plan, i) => {
-              const index = i+1;
+              const index = i + 1;
               return (
-              <DataListItem
-                key={index}
-                isExpanded={false}
-                aria-labelledby="simple-item1"
-              >
-                <DataListCell width={1}>
-                  {/* TODO: <ClusterStatusIcon isSuccessful /> */}
-                  <span >{plan.planName}</span>
-                </DataListCell>
-                <DataListCell width={1}>
-                  {/* TODO: mig logo */}
-                  <span>{plan.migrations.length}</span>
-                </DataListCell>
-                <DataListCell width={1}>
-                  <span>{plan.sourceCluster}</span>
-                </DataListCell>
-                <DataListCell width={1}>
-                  <span>{plan.targetCluster}</span>
-                </DataListCell>
-                <DataListCell width={1}>
-                  <span>{plan.selectedStorage}</span>
-                </DataListCell>
-                <DataListCell width={1}>
-                  <span>{plan.persistentVolumes.length}</span>
-                </DataListCell>
-                <DataListCell width={1}>
-                  {statusComponent(plan)}
-                </DataListCell>
-                <DataListCell width={2}>
-                  <Flex justifyContent="flex-end">
-                    <Box mx={1}>
-                      <Button
-                        variant="primary"
-                        onClick={() => props.onStageTriggered(plan)}
+                <DataListItem
+                  key={index}
+                  isExpanded={false}
+                  aria-labelledby="simple-item1"
+                >
+                  <DataListCell width={1}>
+                    {/* TODO: <ClusterStatusIcon isSuccessful /> */}
+                    <span >{plan.planName}</span>
+                  </DataListCell>
+                  <DataListCell width={1}>
+                    {/* TODO: mig logo */}
+                    <span>{plan.migrations.length}</span>
+                  </DataListCell>
+                  <DataListCell width={1}>
+                    <span>{plan.sourceCluster}</span>
+                  </DataListCell>
+                  <DataListCell width={1}>
+                    <span>{plan.targetCluster}</span>
+                  </DataListCell>
+                  <DataListCell width={1}>
+                    <span>{plan.selectedStorage}</span>
+                  </DataListCell>
+                  <DataListCell width={1}>
+                    <span>{plan.persistentVolumes.length}</span>
+                  </DataListCell>
+                  <DataListCell width={1}>
+                    {statusComponent(plan)}
+                  </DataListCell>
+                  <DataListCell width={2}>
+                    <Flex justifyContent="flex-end">
+                      <Box mx={1}>
+                        <Button
+                          variant="primary"
+                          onClick={() => props.onStageTriggered(plan)}
                         >
-                        Stage
+                          Stage
                       </Button>
-                    </Box>
-                    <Box mx={1}>
-                      <Button
-                        onClick={() => props.onMigrateTriggered(plan)}
-                        variant="primary"
-                      >
-                        Migrate
+                      </Box>
+                      <Box mx={1}>
+                        <MigrateModal
+                          trigger={
+                            <Button
+                              // onClick={() => props.onMigrateTriggered(plan)}
+                              variant="primary"
+                            >
+                              Migrate
                       </Button>
-                    </Box>
-                  </Flex>
-                </DataListCell>
-              </DataListItem>
-            )
-          })}
+                          }
+                        >
+
+                        </MigrateModal>
+
+                      </Box>
+                    </Flex>
+                  </DataListCell>
+                </DataListItem>
+              )
+            })}
           </DataList>
         ) : (
             <Flex alignItems="center" justifyContent="center">
               <Box>
 
-              <EmptyStateComponent type='plan' />
+                <EmptyStateComponent type='plan' />
               </Box>
             </Flex>
           )}
@@ -119,11 +126,11 @@ function statusComponent(plan) {
     plan.status.state === 'Not Started' ||
     plan.status.state === 'Staged Successfully';
 
-  if(printState) {
+  if (printState) {
     statusComponent = (
       <span>{plan.status.state}</span>
     )
-  } else if(plan.status.state === 'Staging') {
+  } else if (plan.status.state === 'Staging') {
     statusComponent = (
       <div>
         <div>{plan.status.state}</div>
