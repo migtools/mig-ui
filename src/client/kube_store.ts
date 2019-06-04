9abdeedd733c6d80d4b9175af92390e9d4bedb35
Ian Bolton <ibolton@redhat.com>
2019-06-03 21:10:56 -0400

    revert kubestore

--- src/client/kube_store.ts
+++ src/client/kube_store.ts
@@ -3,165 +3,11 @@ import { NamespacedResource, ClusterResource, KubeResource } from './resources';
 
 export default class KubeStore {
   private static instance: KubeStore;
-  // public db: any = {
-  //   namespace: {},
-  //   cluster: {},
-  // };
   public db: any = {
-    namespace: {
-      mig: {
-        '/v1/secrets': {
-          s1: {
-            apiVersion: 'v1',
-            data: {
-              'aws-access-key-id': 'ZmRzYQ==',
-              'aws-secret-access-key-id': 'ZmRhcw==',
-            },
-            kind: 'Secret',
-            metadata: {
-              name: 's1',
-              namespace: 'mig',
-            },
-            type: 'Opaque',
-          },
-          c1: {
-            apiVersion: 'v1',
-            data: {
-              saToken: 'ZmRhcw==',
-            },
-            kind: 'Secret',
-            metadata: {
-              name: 'c1',
-              namespace: 'mig',
-            },
-            type: 'Opaque',
-          },
-          c2: {
-            apiVersion: 'v1',
-            data: {
-              saToken: 'ZmRzYQ==',
-            },
-            kind: 'Secret',
-            metadata: {
-              name: 'c2',
-              namespace: 'mig',
-            },
-            type: 'Opaque',
-          },
-        },
-        'migration.openshift.io/v1alpha1/migstorages': {
-          s1: {
-            apiVersion: 'migration.openshift.io/v1alpha1',
-            kind: 'MigStorage',
-            metadata: {
-              name: 's1',
-              namespace: 'mig',
-            },
-            spec: {
-              backupStorageProvider: 'aws',
-              volumeSnapshotProvider: 'aws',
-              backupStorageConfig: {
-                awsBucketName: 'TestingName',
-                awsRegion: 'USA',
-                credsSecretRef: {
-                  name: 's1',
-                  namespace: 'mig',
-                },
-              },
-              volumeSnapshotConfig: {
-                awsRegion: ' ',
-                credsSecretRef: {
-                  name: 's1',
-                  namespace: 'mig',
-                },
-              },
-            },
-          },
-        },
-        'clusterregistry.k8s.io/v1alpha1/clusters': {
-          c1: {
-            apiVersion: 'clusterregistry.k8s.io/v1alpha1',
-            kind: 'Cluster',
-            metadata: {
-              name: 'c1',
-              namespace: 'mig',
-            },
-            spec: {
-              kubernetesApiEndpoints: {
-                serverEndpoints: [
-                  {
-                    clientCIDR: '0.0.0.0',
-                    serverAddress: 'localhost:9000',
-                  },
-                ],
-              },
-            },
-          },
-          c2: {
-            apiVersion: 'clusterregistry.k8s.io/v1alpha1',
-            kind: 'Cluster',
-            metadata: {
-              name: 'c2',
-              namespace: 'mig',
-            },
-            spec: {
-              kubernetesApiEndpoints: {
-                serverEndpoints: [
-                  {
-                    clientCIDR: '0.0.0.0',
-                    serverAddress: 'localhost:9000',
-                  },
-                ],
-              },
-            },
-          },
-        },
-        'migration.openshift.io/v1alpha1/migclusters': {
-          c1: {
-            apiVersion: 'migration.openshift.io/v1alpha1',
-            kind: 'MigCluster',
-            metadata: {
-              name: 'c1',
-              namespace: 'mig',
-            },
-            spec: {
-              isHostCluster: false,
-              clusterRef: {
-                name: 'c1',
-                namespace: 'mig',
-              },
-              serviceAccountSecretRef: {
-                name: 'c1',
-                namespace: 'mig',
-              },
-            },
-          },
-          c2: {
-            apiVersion: 'migration.openshift.io/v1alpha1',
-            kind: 'MigCluster',
-            metadata: {
-              name: 'c2',
-              namespace: 'mig',
-            },
-            spec: {
-              isHostCluster: false,
-              clusterRef: {
-                name: 'c2',
-                namespace: 'mig',
-              },
-              serviceAccountSecretRef: {
-                name: 'c2',
-                namespace: 'mig',
-              },
-            },
-          },
-        },
-      },
-    },
-    cluster: {
-      '/v1/namespaces': {},
-    },
+    namespace: {},
+    cluster: {},
   };
+<<<<<<< HEAD
   // public db: any =
   //   {
   //     namespace: {
@@ -346,6 +192,163 @@ export default class KubeStore {
   //       "/v1/namespaces": {}
   //     }
   //   }
+=======
+  // public db: any = {
+  //   namespace: {
+  //     mig: {
+  //       '/v1/secrets': {
+  //         s1: {
+  //           apiVersion: 'v1',
+  //           data: {
+  //             'aws-access-key-id': 'ZmRzYQ==',
+  //             'aws-secret-access-key-id': 'ZmRhcw==',
+  //           },
+  //           kind: 'Secret',
+  //           metadata: {
+  //             name: 's1',
+  //             namespace: 'mig',
+  //           },
+  //           type: 'Opaque',
+  //         },
+  //         c1: {
+  //           apiVersion: 'v1',
+  //           data: {
+  //             saToken: 'ZmRhcw==',
+  //           },
+  //           kind: 'Secret',
+  //           metadata: {
+  //             name: 'c1',
+  //             namespace: 'mig',
+  //           },
+  //           type: 'Opaque',
+  //         },
+  //         c2: {
+  //           apiVersion: 'v1',
+  //           data: {
+  //             saToken: 'ZmRzYQ==',
+  //           },
+  //           kind: 'Secret',
+  //           metadata: {
+  //             name: 'c2',
+  //             namespace: 'mig',
+  //           },
+  //           type: 'Opaque',
+  //         },
+  //       },
+  //       'migration.openshift.io/v1alpha1/migstorages': {
+  //         s1: {
+  //           apiVersion: 'migration.openshift.io/v1alpha1',
+  //           kind: 'MigStorage',
+  //           metadata: {
+  //             name: 's1',
+  //             namespace: 'mig',
+  //           },
+  //           spec: {
+  //             backupStorageProvider: 'aws',
+  //             volumeSnapshotProvider: 'aws',
+  //             backupStorageConfig: {
+  //               awsBucketName: 'TestingName',
+  //               awsRegion: 'USA',
+  //               credsSecretRef: {
+  //                 name: 's1',
+  //                 namespace: 'mig',
+  //               },
+  //             },
+  //             volumeSnapshotConfig: {
+  //               awsRegion: ' ',
+  //               credsSecretRef: {
+  //                 name: 's1',
+  //                 namespace: 'mig',
+  //               },
+  //             },
+  //           },
+  //         },
+  //       },
+  //       'clusterregistry.k8s.io/v1alpha1/clusters': {
+  //         c1: {
+  //           apiVersion: 'clusterregistry.k8s.io/v1alpha1',
+  //           kind: 'Cluster',
+  //           metadata: {
+  //             name: 'c1',
+  //             namespace: 'mig',
+  //           },
+  //           spec: {
+  //             kubernetesApiEndpoints: {
+  //               serverEndpoints: [
+  //                 {
+  //                   clientCIDR: '0.0.0.0',
+  //                   serverAddress: 'localhost:9000',
+  //                 },
+  //               ],
+  //             },
+  //           },
+  //         },
+  //         c2: {
+  //           apiVersion: 'clusterregistry.k8s.io/v1alpha1',
+  //           kind: 'Cluster',
+  //           metadata: {
+  //             name: 'c2',
+  //             namespace: 'mig',
+  //           },
+  //           spec: {
+  //             kubernetesApiEndpoints: {
+  //               serverEndpoints: [
+  //                 {
+  //                   clientCIDR: '0.0.0.0',
+  //                   serverAddress: 'localhost:9000',
+  //                 },
+  //               ],
+  //             },
+  //           },
+  //         },
+  //       },
+  //       'migration.openshift.io/v1alpha1/migclusters': {
+  //         c1: {
+  //           apiVersion: 'migration.openshift.io/v1alpha1',
+  //           kind: 'MigCluster',
+  //           metadata: {
+  //             name: 'c1',
+  //             namespace: 'mig',
+  //           },
+  //           spec: {
+  //             isHostCluster: false,
+  //             clusterRef: {
+  //               name: 'c1',
+  //               namespace: 'mig',
+  //             },
+  //             serviceAccountSecretRef: {
+  //               name: 'c1',
+  //               namespace: 'mig',
+  //             },
+  //           },
+  //         },
+  //         c2: {
+  //           apiVersion: 'migration.openshift.io/v1alpha1',
+  //           kind: 'MigCluster',
+  //           metadata: {
+  //             name: 'c2',
+  //             namespace: 'mig',
+  //           },
+  //           spec: {
+  //             isHostCluster: false,
+  //             clusterRef: {
+  //               name: 'c2',
+  //               namespace: 'mig',
+  //             },
+  //             serviceAccountSecretRef: {
+  //               name: 'c2',
+  //               namespace: 'mig',
+  //             },
+  //           },
+  //         },
+  //       },
+  //     },
+  //   },
+  //   cluster: {
+  //     '/v1/namespaces': {},
+  //   },
+  // };
+>>>>>>> revert kubestore
 
   public static get Instance() {
     return this.instance || (this.instance = new this());
