044da586000797e1367e6dac7f5f1ac663b609f6
Andy Veselov <aveselov@redhat.com>
2019-06-04 19:58:55 +0200

    Add button validation test (#220)
    * Add test for cluster buttons disabled
    
    * Update gitignore

--- cypress/integration/cluster_spec.js
+++ cypress/integration/cluster_spec.js
@@ -15,4 +15,20 @@ describe('Cluster functionality test', () => {
     });
     cy.contains('test cluster');
   });
+  // TODO: add tests for update and delete cluster, after
+  // https://github.com/fusor/mig-ui/pull/211#issuecomment-498383188 will be fixed
+  it('has buttons disabled when input data is missing', () => {
+    cy.visit('/');
+    cy.get('#add-cluster-btn').then(() => {
+      cy.get('#add-cluster-btn')
+        .first()
+        .click()
+        .then(() => {
+          cy.get('#url-input').type('example.com');
+          cy.get('#token-input').type('123456');
+          cy.get('#check-connection-btn').should('be.disabled');
+          cy.get('#submit-cluster-btn').should('be.disabled');
+        });
+    });
+  });
 });
