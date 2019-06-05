dd33b5c0e58e169dcb34a0ed331ff1642509defe
Andrei Veselov <aveselov@redhat.com>
2019-06-05 15:32:39 +0200

    Small refactor for cluster spec

--- cypress/integration/cluster_spec.js
+++ cypress/integration/cluster_spec.js
@@ -1,34 +1,44 @@
 describe('Cluster functionality test', () => {
-  it('adds cluster with valid data', () => {
+  beforeEach(() => {
     cy.visit('/');
-    cy.get('#add-cluster-btn').then(() => {
-      cy.get('#add-cluster-btn')
-        .first()
-        .click()
-        .then(() => {
-          cy.get('#cluster-name-input').type('test cluster');
-          cy.get('#url-input').type('example.com');
-          cy.get('#token-input').type('123456');
-          cy.get('#check-connection-btn').click();
-          cy.get('#submit-cluster-btn').click();
-        });
+  });
+
+  context('Add cluster', () => {
+    const clusterName = 'test cluster';
+    const url = 'example.com';
+    const token = '123456';
+
+    it('adds cluster with valid data', () => {
+      cy.get('#add-cluster-btn').then(() => {
+        cy.get('#add-cluster-btn')
+          .first()
+          .click()
+          .then(() => {
+            cy.get('#cluster-name-input').type(clusterName);
+            cy.get('#url-input').type(url);
+            cy.get('#token-input').type(token);
+            cy.get('#check-connection-btn').click();
+            cy.get('#submit-cluster-btn').click();
+          });
+      });
+      cy.contains('test cluster');
     });
-    cy.contains('test cluster');
+
+    it('has buttons disabled when input data is missing', () => {
+      cy.get('#add-cluster-btn').then(() => {
+        cy.get('#add-cluster-btn')
+          .first()
+          .click()
+          .then(() => {
+            cy.get('#cluster-name-input').should('have.value', '');
+            cy.get('#url-input').type(url);
+            cy.get('#token-input').type(token);
+            cy.get('#check-connection-btn').should('be.disabled');
+            cy.get('#submit-cluster-btn').should('be.disabled');
+          });
+      });
+    });
   });
   // TODO: add tests for update and delete cluster, after
   // https://github.com/fusor/mig-ui/pull/211#issuecomment-498383188 will be fixed
-  it('has buttons disabled when input data is missing', () => {
-    cy.visit('/');
-    cy.get('#add-cluster-btn').then(() => {
-      cy.get('#add-cluster-btn')
-        .first()
-        .click()
-        .then(() => {
-          cy.get('#url-input').type('example.com');
-          cy.get('#token-input').type('123456');
-          cy.get('#check-connection-btn').should('be.disabled');
-          cy.get('#submit-cluster-btn').should('be.disabled');
-        });
-    });
-  });
 });
