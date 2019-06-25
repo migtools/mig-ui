e9b8dddde207e6b01aff005f4ee1dbd801fa6d20
Erik Nelson <erik@nsk.io>
2019-06-25 12:22:39 -0400

    Update text

--- src/app/auth/CertErrorComponent.tsx
+++ src/app/auth/CertErrorComponent.tsx
@@ -14,9 +14,12 @@ class CertErrorComponent extends React.Component<any> {
       <div>
         {this.props.certError ?
           <div>
-            A certificate error has occurred, please visit and accept the cert, 
-            or correctly install your self-signed CA:
+            A certificate error has occurred, likely due to the usage of self 
+            signed certificates in one of the clusters. Please try to visit the failed url 
+            and accept the CA: 
             <a href={this.props.certError.failedUrl}>  {this.props.certError.failedUrl}</a>
+            <div />
+            The correct way to address this is to install your self CA into your browser.
             <div />
             NOTE: The contents of the resulting page may report "unauthorized". 
             This is expected. After accepting the certificate, please reload the app.
