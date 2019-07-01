6640f7e3389491441d8eb4bc2ff219e3254482d6
Andrei Veselov <aveselov@redhat.com>
2019-07-01 18:48:14 +0200

    Add patchResource function

--- src/client/kube_store/index.ts
+++ src/client/kube_store/index.ts
@@ -3,7 +3,7 @@ import { NamespacedResource, ClusterResource, KubeResource,
   CoreNamespacedResource, CoreClusterResource, } from '../resources';
 
 import mocked_data from './mocked_data';
-
+import JsonMergePatch from 'json-merge-patch';
 const localStorageMockedDataKey = 'CAM_MOCKED_DATA';
 
 export default class KubeStore {
@@ -14,192 +14,6 @@ export default class KubeStore {
     namespace: {},
     cluster: {},
   };
-
-
-  // public db: any =
-  //   {
-  //     namespace: {
-  //       mig: {
-  //         "/v1/secrets": {
-  //           s1: {
-  //             apiVersion: "v1",
-  //             data: {
-  //               "aws-access-key-id": "ZmRzYQ==",
-  //               "aws-secret-access-key-id": "ZmRhcw=="
-  //             },
-  //             kind: "Secret",
-  //             metadata: {
-  //               name: "s1",
-  //               namespace: "mig"
-  //             },
-  //             type: "Opaque"
-  //           },
-  //           c1: {
-  //             apiVersion: "v1",
-  //             data: {
-  //               saToken: "ZmRhcw=="
-  //             },
-  //             kind: "Secret",
-  //             metadata: {
-  //               name: "c1",
-  //               namespace: "mig"
-  //             },
-  //             type: "Opaque"
-  //           },
-  //           c2: {
-  //             apiVersion: "v1",
-  //             data: {
-  //               saToken: "ZmRzYQ=="
-  //             },
-  //             kind: "Secret",
-  //             metadata: {
-  //               name: "c2",
-  //               namespace: "mig"
-  //             },
-  //             type: "Opaque"
-  //           }
-  //         },
-  //         "migration.openshift.io/v1alpha1/migstorages": {
-  //           s1: {
-  //             apiVersion: "migration.openshift.io/v1alpha1",
-  //             kind: "MigStorage",
-  //             metadata: {
-  //               name: "s1",
-  //               namespace: "mig"
-  //             },
-  //             spec: {
-  //               backupStorageProvider: "aws",
-  //               volumeSnapshotProvider: "aws",
-  //               backupStorageConfig: {
-  //                 awsBucketName: "TestingName",
-  //                 awsRegion: "USA",
-  //                 credsSecretRef: {
-  //                   name: "s1",
-  //                   namespace: "mig"
-  //                 }
-  //               },
-  //               volumeSnapshotConfig: {
-  //                 awsRegion: " ",
-  //                 credsSecretRef: {
-  //                   name: "s1",
-  //                   namespace: "mig"
-  //                 }
-  //               }
-  //             }
-  //           }
-  //         },
-  //         "clusterregistry.k8s.io/v1alpha1/clusters": {
-  //           c1: {
-  //             apiVersion: "clusterregistry.k8s.io/v1alpha1",
-  //             kind: "Cluster",
-  //             metadata: {
-  //               name: "c1",
-  //               namespace: "mig"
-  //             },
-  //             spec: {
-  //               kubernetesApiEndpoints: {
-  //                 serverEndpoints: [
-  //                   {
-  //                     clientCIDR: "0.0.0.0",
-  //                     serverAddress: "localhost:9000"
-  //                   }
-  //                 ]
-  //               }
-  //             }
-  //           },
-  //           c2: {
-  //             apiVersion: "clusterregistry.k8s.io/v1alpha1",
-  //             kind: "Cluster",
-  //             metadata: {
-  //               name: "c2",
-  //               namespace: "mig"
-  //             },
-  //             spec: {
-  //               kubernetesApiEndpoints: {
-  //                 serverEndpoints: [
-  //                   {
-  //                     clientCIDR: "0.0.0.0",
-  //                     serverAddress: "localhost:9000"
-  //                   }
-  //                 ]
-  //               }
-  //             }
-  //           }
-  //         },
-  //         "migration.openshift.io/v1alpha1/migclusters": {
-  //           c1: {
-  //             apiVersion: "migration.openshift.io/v1alpha1",
-  //             kind: "MigCluster",
-  //             metadata: {
-  //               name: "c1",
-  //               namespace: "mig"
-  //             },
-  //             spec: {
-  //               isHostCluster: false,
-  //               clusterRef: {
-  //                 name: "c1",
-  //                 namespace: "mig"
-  //               },
-  //               serviceAccountSecretRef: {
-  //                 name: "c1",
-  //                 namespace: "mig"
-  //               }
-  //             }
-  //           },
-  //           c2: {
-  //             apiVersion: "migration.openshift.io/v1alpha1",
-  //             kind: "MigCluster",
-  //             metadata: {
-  //               name: "c2",
-  //               namespace: "mig"
-  //             },
-  //             spec: {
-  //               isHostCluster: false,
-  //               clusterRef: {
-  //                 name: "c2",
-  //                 namespace: "mig"
-  //               },
-  //               serviceAccountSecretRef: {
-  //                 name: "c2",
-  //                 namespace: "mig"
-  //               }
-  //             }
-  //           }
-  //         },
-  //         "migration.openshift.io/v1alpha1/migplans": {
-  //           plan1: {
-  //             apiVersion: "migration.openshift.io/v1alpha1",
-  //             kind: "MigPlan",
-  //             metadata: {
-  //               name: "plan1",
-  //               namespace: "mig"
-  //             },
-  //             spec: {
-  //               srcMigClusterRef: {
-  //                 name: "c1",
-  //                 namespace: "mig"
-  //               },
-  //               destMigClusterRef: {
-  //                 name: "c2",
-  //                 namespace: "mig"
-  //               },
-  //               migStorageRef: {
-  //                 name: "s1",
-  //                 namespace: "mig"
-  //               },
-  //               migAssetCollectionRef: {
-  //                 name: "temp asset name",
-  //                 namespace: "mig"
-  //               }
-  //             }
-  //           }
-  //         }
-  //       }
-  //     },
-  //     cluster: {
-  //       "/v1/namespaces": {}
-  //     }
-  //   }
 
   public constructor(clusterName) {
     this.clusterName = clusterName;
@@ -231,6 +45,48 @@ export default class KubeStore {
 
   public static Instance() {
     return this.instance || (this.instance = new this(''));
+  }
+
+  public patchResource(resource: KubeResource, name: string, patch: object): object {
+    let result: object = {};
+    let key: string = '';
+
+    if (resource instanceof NamespacedResource) {
+      const namespacedResource = resource as NamespacedResource;
+      if (resource instanceof CoreNamespacedResource) {
+        // Core Namespaced
+        key = `api/${resource.gvk().version}/namespaces/${namespacedResource.namespace}/${resource.gvk().kindPlural}`;
+        if (this.data()[key]) {
+          result = this.data()[key][name];
+        }
+      } else {
+        // Extension Namespaced
+        key = `apis/${resource.gvk().group}/${resource.gvk().version}` +
+          `/namespaces/${namespacedResource.namespace}/${resource.gvk().kindPlural}`;
+        if (this.data()[key]) {
+          result = this.data()[key][name];
+        }
+      }
+    } else {
+      if (resource instanceof CoreClusterResource) {
+        // Core ClusterResource
+        key = `api/${resource.gvk().version}/namespaces/${resource.gvk().kindPlural}`;
+        if (this.data()[key]) {
+          result = this.data()[key][name];
+        }
+      } else {
+        // Extension ClusterResource
+        key = `apis/${resource.gvk().group}/${resource.gvk().version}/${resource.gvk().kindPlural}`;
+        if (this.data()[key]) {
+          result = this.data()[key][name];
+        }
+      }
+    }
+
+    const patchedObject = JsonMergePatch.apply(resource, patch);
+    localStorage[key] = patchedObject;
+// console.log(patchedObject)
+    return resource;
   }
 
   public setResource(resource: KubeResource, name: string, newObject: object): object {
