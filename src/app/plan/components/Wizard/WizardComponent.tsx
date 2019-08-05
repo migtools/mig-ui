3dd91be0706fd56e744e167ac949d8cac823226d
Ian Bolton <ibolton@redhat.com>
2019-08-05 11:36:56 -0400

    remove useRef

--- src/app/plan/components/Wizard/WizardComponent.tsx
+++ src/app/plan/components/Wizard/WizardComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Wizard } from '@patternfly/react-core';
 import GeneralForm from './GeneralForm';
 import ResourceSelectForm from './ResourceSelectForm';
