59bd49223f53978230d262d106e56487ac99c61f
Ian Bolton <ibolton@redhat.com>
2019-08-14 13:31:39 -0400

    fix botched rebase

--- src/app/plan/duck/selectors.ts
+++ src/app/plan/duck/selectors.ts
@@ -10,6 +10,7 @@ const getMigMeta = state => state.migMeta;
 const getPlansWithStatus = createSelector(
   [planSelector],
   plans => {
+
     const plansWithStatus = plans.map(plan => {
       let hasReadyCondition = null;
       let hasPlanError = null;
@@ -23,50 +24,6 @@ const getPlansWithStatus = createSelector(
       let hasMigrationError = null;
       let latestType = null;
       let latestIsFailed = false;
-      if (plan.MigPlan.status) {
-        hasClosedCondition = plan.MigPlan.spec.closed;
-        hasReadyCondition = !!plan.MigPlan.status.conditions.filter(c => c.type === 'Ready').length;
-        hasPlanError = !!plan.MigPlan.status.conditions.filter(c => c.category === 'Critical')
-          .length;
-
-        if (plan.Migrations.length) {
-          const latest = plan.Migrations[0];
-
-          hasPrevMigrations = !!plan.Migrations.length;
-          latestType = latest.spec.stage ? 'Stage' : 'Migration';
-
-          if (latest.status && latest.status.conditions) {
-            latestIsFailed = !!(latest.status.conditions.some(c => c.type === 'Failed'));
-          }
-
-          hasSucceededStage = !!plan.Migrations.filter(m => {
-            if (m.status && m.spec.stage) {
-              return m.status.conditions.some(c => c.type === 'Succeeded');
-            }
-          }).length;
-
-          hasSucceededMigration = !!plan.Migrations.filter(m => {
-            if (m.status && !m.spec.stage) {
-              return m.status.conditions.some(c => c.type === 'Succeeded');
-            }
-          }).length;
-
-          hasAttemptedMigration = !!plan.Migrations.some(m => !m.spec.stage);
-
-          finalMigrationComplete = !!plan.Migrations.filter(m => {
-            if (m.status) {
-              return m.spec.stage === false && hasSucceededMigration;
-            }
-          }).length;
-
-          hasRunningMigrations = !!plan.Migrations.filter(m => {
-            if (m.status) {
-              return m.status.conditions.some(c => c.type === 'Running');
-            }
-          }).length;
-        }
-      }
-
       if (!plan.MigPlan.status || !plan.MigPlan.status.conditions) {
         const emptyStatusObject = {
           hasSucceededStage,
@@ -82,15 +39,20 @@ const getPlansWithStatus = createSelector(
         };
         return { ...plan, PlanStatus: emptyStatusObject };
       }
-
       hasClosedCondition = !!plan.MigPlan.status.conditions.filter(c => c.type === 'Closed').length;
       hasReadyCondition = !!plan.MigPlan.status.conditions.filter(c => c.type === 'Ready').length;
       hasPlanError = !!plan.MigPlan.status.conditions.filter(c => c.category === 'Critical')
         .length;
 
       if (plan.Migrations.length) {
+        const latest = plan.Migrations[0];
+
         hasPrevMigrations = !!plan.Migrations.length;
-        latestType = plan.Migrations[0].spec.stage ? 'Stage' : 'Migration';
+        latestType = latest.spec.stage ? 'Stage' : 'Migration';
+
+        if (latest.status && latest.status.conditions) {
+          latestIsFailed = !!(latest.status.conditions.some(c => c.type === 'Failed'));
+        }
 
         hasSucceededStage = !!plan.Migrations.filter(m => {
           if (m.status && m.spec.stage) {
@@ -104,11 +66,7 @@ const getPlansWithStatus = createSelector(
           }
         }).length;
 
-        hasMigrationError = !!plan.Migrations.filter(m => {
-          if (m.status) {
-            return m.status.conditions.some(c => c.type === 'Failed');
-          }
-        }).length;
+        hasAttemptedMigration = !!plan.Migrations.some(m => !m.spec.stage);
 
         finalMigrationComplete = !!plan.Migrations.filter(m => {
           if (m.status) {
@@ -121,22 +79,22 @@ const getPlansWithStatus = createSelector(
             return m.status.conditions.some(c => c.type === 'Running');
           }
         }).length;
-      }
-      const statusObject = {
-        hasSucceededStage,
-        hasPrevMigrations,
-        hasClosedCondition,
-        hasReadyCondition,
-        hasNotReadyCondition: hasPlanError,
-        hasRunningMigrations,
-        hasSucceededMigration,
-        finalMigrationComplete,
-        hasFailedCondition: hasMigrationError,
-        latestType,
-      };
 
-      return { ...plan, PlanStatus: statusObject };
-    });
+        const statusObject = {
+          hasSucceededStage,
+          hasPrevMigrations,
+          hasClosedCondition,
+          hasReadyCondition,
+          hasNotReadyCondition: hasPlanError,
+          hasRunningMigrations,
+          hasSucceededMigration,
+          finalMigrationComplete,
+          hasFailedCondition: hasMigrationError,
+          latestType,
+        };
+
+        return { ...plan, PlanStatus: statusObject };
+      });
 
     return plansWithStatus;
   }
