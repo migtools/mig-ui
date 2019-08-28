be00b45e334604067e5bbd36dc9ade744aae7e39
Ian Bolton <ibolton@redhat.com>
2019-08-28 15:18:34 -0400

    add deleting planStatus on a plan level

--- src/app/home/components/DataList/Plans/PlanStatus.tsx
+++ src/app/home/components/DataList/Plans/PlanStatus.tsx
@@ -15,32 +15,43 @@ const PlanStatus: React.FunctionComponent<IProps> = ({ plan }) => {
     hasPrevMigrations,
     latestType,
     latestIsFailed,
+    isDeleting
   } = plan.PlanStatus;
 
   const getStatus = () => {
     const status = { text: 'Waiting for status...' };
+    if (isDeleting) {
+      status.text = 'Deleting';
+      return status;
+    }
     if (hasReadyCondition || !hasPrevMigrations) {
       status.text = 'Ready';
+      return status;
     }
     if (hasNotReadyCondition || !hasReadyCondition) {
       status.text = 'Not Ready';
+      return status;
     }
     if (hasSucceededStage) {
       status.text = `Stage Succeeded`;
+      return status;
     }
     if (hasSucceededMigration) {
       status.text = `Migration Succeeded`;
+      return status;
     }
     if (latestIsFailed) {
       status.text = `${latestType} Failed`;
+      return status;
     }
     if (hasRunningMigrations) {
       status.text = `${latestType} Running`;
+      return status;
     }
     if (hasClosedCondition) {
       status.text = 'Closed';
+      return status;
     }
-    return status;
   };
   return (
     <div>
