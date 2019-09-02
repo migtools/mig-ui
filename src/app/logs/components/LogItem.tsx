fc78fd0bbea8f15dd335a96a21a0000fbbd505f3
Ian Bolton <ibolton@redhat.com>
2019-09-02 16:11:09 -0400

    push lint fix

--- src/app/logs/components/LogItem.tsx
+++ src/app/logs/components/LogItem.tsx
@@ -2,21 +2,21 @@
 import { jsx } from '@emotion/core';
 import styled from '@emotion/styled';
 import { Box } from '@rebass/emotion';
-import ReactTable from 'react-table'
-import 'react-table/react-table.css'
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
 const LogItem = ({ log }) => {
   const arrayOfLines = log.split(/\r?\n/);
   const myData = arrayOfLines.map((item, index) => {
-    var rObj = {};
+    const rObj = {};
     const slicedItem = item.slice(1, -1).replace(/['"]+/g, ' ');
-    rObj["value"] = slicedItem;
+    rObj['value'] = slicedItem;
     return rObj;
   }
-  )
+  );
   const columns = [{
     Header: 'Value', accessor: 'value',
-  }]
+  }];
 
   const StyledBox = styled(Box)`
     height: 100%;
@@ -28,13 +28,12 @@ const LogItem = ({ log }) => {
         style
         filterable
         defaultFilterMethod={(filter, row) => {
-          return row[filter.id].includes(filter.value)
+          return row[filter.id].includes(filter.value);
         }
         }
         columns={columns}
-        data={myData} // The data prop should be immutable and only change when you want to update the table
+        data={myData}
       />
-
     </StyledBox>
   );
 };
