e01635a99faddc9bb3d844c793a73f94019a829c
Matt Nolting <mnolting@redhat.com>
2019-09-05 17:30:19 -0400

    refactor(wizard-layout): update step layouts

--- src/app/plan/components/Wizard/VolumesForm.tsx
+++ src/app/plan/components/Wizard/VolumesForm.tsx
@@ -1,9 +1,14 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import { Box } from '@rebass/emotion';
-import { TextContent, TextList, TextListItem } from '@patternfly/react-core';
+import {
+  Grid,
+  GridItem,
+  Text,
+  TextContent,
+  TextVariants,
+} from '@patternfly/react-core';
 import VolumesTable from './VolumesTable';
-import styled from '@emotion/styled';
 const VolumesForm = props => {
   const {
     setFieldValue,
@@ -15,27 +20,29 @@ const VolumesForm = props => {
     pvResourceList,
     isFetchingPVResources
   } = props;
-  const StyledTextContent = styled(TextContent)`
-    margin: 1em 0 1em 0;
-  `;
+
   return (
-    <Box>
-      <StyledTextContent>
-        <TextList component="dl">
-          <TextListItem component="dt">Choose to move or copy persistent volumes:</TextListItem>
-        </TextList>
-      </StyledTextContent>
-      <VolumesTable
-        isPVError={isPVError}
-        isFetchingPVList={isFetchingPVList}
-        setFieldValue={setFieldValue}
-        values={values}
-        currentPlan={currentPlan}
-        getPVResourcesRequest={getPVResourcesRequest}
-        pvResourceList={pvResourceList}
-        isFetchingPVResources={isFetchingPVResources}
-      />
-    </Box>
+    <Grid gutter="md">
+      <GridItem>
+        <TextContent>
+          <Text component={TextVariants.p}>
+            Choose to move or copy persistent volumes:
+          </Text>
+        </TextContent>
+      </GridItem>
+      <GridItem>
+        <VolumesTable
+          isPVError={isPVError}
+          isFetchingPVList={isFetchingPVList}
+          setFieldValue={setFieldValue}
+          values={values}
+          currentPlan={currentPlan}
+          getPVResourcesRequest={getPVResourcesRequest}
+          pvResourceList={pvResourceList}
+          isFetchingPVResources={isFetchingPVResources}
+        />
+       </GridItem>
+    </Grid>
   );
 };
 export default VolumesForm;
