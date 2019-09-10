3df94e293cf84e8d250620890a83e3f0824bf7fc
Matt Nolting <matthew.nolting@gmail.com>
2019-09-10 10:42:23 -0400

    refactor(migration-table): update table layout and css (#534)
    * refactor(migration-table): update table layout and css
    
    * refactor(migration-table): add double quotes
    
    * cleanup comments and ts-ignore

--- src/app/home/components/DataList/Plans/PlanActions.tsx
+++ src/app/home/components/DataList/Plans/PlanActions.tsx
@@ -1,15 +1,15 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import React, { useState, useContext } from 'react';
+import { useState, useContext } from 'react';
 import { PlanContext } from '../../../duck/context';
-import { Button, Dropdown, DropdownItem, KebabToggle } from '@patternfly/react-core';
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownPosition,
+  KebabToggle
+} from '@patternfly/react-core';
 import { useOpenModal } from '../../../duck/hooks';
-import { Flex, Box } from '@rebass/emotion';
-import PlanStatus from './PlanStatus';
 import MigrateModal from '../../../../plan/components/MigrateModal';
-import theme from '../../../../../theme';
-import Loader from 'react-loader-spinner';
-import { css } from '@emotion/core';
 import { Link } from 'react-router-dom';
 
 const PlanActions = ({ plan }) => {
@@ -21,13 +21,44 @@ const PlanActions = ({ plan }) => {
     hasErrorCondition,
     hasRunningMigrations,
     hasAttemptedMigration,
-    hasSucceededMigration,
     finalMigrationComplete,
     isPlanLocked
   } = plan.PlanStatus;
 
   const [kebabIsOpen, setKebabIsOpen] = useState(false);
   const kebabDropdownItems = [
+    <DropdownItem
+      onClick={() => {
+        planContext.handleStageTriggered(plan);
+      }}
+      key="stagePlan"
+      isDisabled={
+        hasClosedCondition ||
+        !hasReadyCondition ||
+        hasErrorCondition ||
+        hasRunningMigrations ||
+        hasAttemptedMigration ||
+        finalMigrationComplete ||
+        isPlanLocked
+      }
+    >
+      Stage
+    </DropdownItem>,
+    <DropdownItem
+      onClick={toggleOpen}
+      key="migratePlan"
+      isDisabled={
+        hasClosedCondition ||
+        !hasReadyCondition ||
+        hasErrorCondition ||
+        hasRunningMigrations ||
+        finalMigrationComplete ||
+        isPlanLocked
+      }
+    >
+      Migrate
+      <MigrateModal plan={plan} isOpen={isOpen} onHandleClose={toggleOpen} />
+    </DropdownItem>,
     <DropdownItem
       onClick={() => {
         planContext.handleDeletePlan(plan);
@@ -53,48 +84,8 @@ const PlanActions = ({ plan }) => {
   ];
 
   return (
-    <Flex>
-      <Box m="auto auto auto 0">
-        <PlanStatus plan={plan} />
-      </Box>
-      <Box mx={1}>
-        <Button
-          isDisabled={
-            hasClosedCondition ||
-            !hasReadyCondition ||
-            hasErrorCondition ||
-            hasRunningMigrations ||
-            hasAttemptedMigration ||
-            finalMigrationComplete ||
-            isPlanLocked
-          }
-          variant="primary"
-          onClick={() => {
-            planContext.handleStageTriggered(plan);
-          }}
-        >
-          Stage
-        </Button>
-      </Box>
-      <Box mx={1}>
-        <Button
-          isDisabled={
-            hasClosedCondition ||
-            !hasReadyCondition ||
-            hasErrorCondition ||
-            hasRunningMigrations ||
-            finalMigrationComplete ||
-            isPlanLocked
-          }
-          variant="primary"
-          onClick={toggleOpen}
-        >
-          Migrate
-        </Button>
-        <MigrateModal plan={plan} isOpen={isOpen} onHandleClose={toggleOpen} />
-      </Box>
-
-      <Box margin="auto 4px" >
+    <div className="pf-l-flex pf-m-nowrap">
+      <div className="pf-l-flex__item pf-m-align-right">
         <Dropdown
           toggle={<KebabToggle
             onToggle={() => setKebabIsOpen(!kebabIsOpen)}
@@ -102,21 +93,10 @@ const PlanActions = ({ plan }) => {
           isOpen={kebabIsOpen}
           isPlain
           dropdownItems={kebabDropdownItems}
+          position={DropdownPosition.right}
         />
-      </Box>
-      {hasRunningMigrations && (
-        <Box
-          css={css`
-            height: 100%;
-            text-align: center;
-            margin: auto 4px auto 4px;
-            width: 1em;
-          `}
-        >
-          <Loader type="ThreeDots" color={theme.colors.navy} height="100%" width="100%" />
-        </Box>
-      )}
-    </Flex>
+      </div>
+    </div>
   );
 };
 
