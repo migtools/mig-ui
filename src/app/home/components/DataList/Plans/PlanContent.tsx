e49c19af95b7512b2ec9936f3dfe4fdabd0e8d5a
Ian Bolton <ibolton@redhat.com>
2019-09-18 10:52:12 -0400

    add edit toggle for wizard

--- src/app/home/components/DataList/Plans/PlanContent.tsx
+++ src/app/home/components/DataList/Plans/PlanContent.tsx
@@ -19,7 +19,7 @@ interface IProps {
   storageList: any;
   isExpanded: boolean;
   plansDisabled: boolean;
-  toggleOpen: () => void;
+  toggleWizardOpen: () => void;
 }
 
 const columns = [
@@ -42,132 +42,133 @@ const columns = [
   ''
 ];
 
-const buildNewRows = (
-  currentRows, planList,
-) => {
-  const newRows = planList.map((plan, planIndex) => {
-    const MigrationsIcon = () => {
-      if (plan.Migrations.length > 0) {
-        return <span className="pf-c-icon pf-m-info"><ServiceIcon /></span>;
-      } else {
-        return <span className="pf-c-icon"><ServiceIcon /></span>;
-      }
-    };
-    const parentIndex = planIndex * 2;
-    const planName = plan.MigPlan.metadata.name;
-    const planKey = `${planName}-${planIndex}`;
-
-    //check previous expanded value
-    let isOpenPrev = null;
-    if (currentRows.length > 0) {
-      const matchingIndex = currentRows.filter((_row, i) => i === parentIndex);
-      if (matchingIndex[0] && matchingIndex[0].cells.length > 0) {
-        isOpenPrev = matchingIndex[0].cells[1].props.isOpen;
-      }
-    }
-
-    const migStorageName = plan.MigPlan.spec.migStorageRef ?
-      plan.MigPlan.spec.migStorageRef.name : 'N/A';
-
-    const pvCount = plan.MigPlan.spec.persistentVolumes ?
-      plan.MigPlan.spec.persistentVolumes.length : 0;
-    return [
-      {
-        cells: [
-          {
-            title: (
-              <div className="pf-l-flex">
-                <div className="pf-l-flex__item">
-                  <span>{plan.MigPlan.metadata.name}</span>
-                </div>
-              </div>
-            ),
-
-            props: { component: 'th' },
-          },
-          {
-            title: (
-              <div className="pf-l-flex">
-                <div className="pf-l-flex__item" key={planKey + '-icon'}>
-                  <MigrationsIcon />
-                </div>
-                <div className="pf-l-flex__item" key={planKey + '-text'}>
-                  <span>{plan.Migrations.length || 0}</span>
-                </div>
-              </div>
-            ),
-
-            props: {
-              isOpen: isOpenPrev || false,
-              ariaControls: 'migrations-history-expansion-table',
-            },
-          },
-          {
-            title: <span>{plan.MigPlan.spec.srcMigClusterRef.name}</span>,
-          },
-          {
-            title: <span>{plan.MigPlan.spec.destMigClusterRef.name}</span>,
-          },
-          {
-            title: <span>{migStorageName}</span>,
-          },
-          {
-            title: (
-              <div className="pf-l-flex">
-                <div className="pf-l-flex__item">
-                  <DatabaseIcon />
-                </div>
-                <div className="pf-l-flex__item">
-                  <span>{pvCount}</span>
-                </div>
-              </div>
-            ),
-          },
-          {
-            title: <PlanStatus
-              plan={plan}
-            />,
-          },
-          {
-            title: <PlanActions
-              plan={plan}
-            />,
-
-            props: {
-              className: 'pf-c-table__action',
-            }
-          },
-        ],
-      },
-      {
-        parent: parentIndex,
-        compoundParent: 1,
-        cells: [
-          {
-            title: (
-              <MigrationsTable
-                type="Migrations"
-                migrations={plan.Migrations}
-                isPlanLocked={plan.PlanStatus.isPlanLocked}
-                id="migrations-history-expansion-table"
-              />
-            ),
-            props: { colSpan: 9, className: 'pf-m-no-padding' },
-          },
-        ],
-      },
-    ];
-  });
-
-  return flatten(newRows);
-};
 
 const PlanContent: React.FunctionComponent<IProps> = ({
   planList,
   isExpanded,
   plansDisabled,
-  toggleOpen,
+  toggleWizardOpen,
 }) => {
+  const buildNewRows = (
+    currentRows, planList,
+  ) => {
+    const newRows = planList.map((plan, planIndex) => {
+      const MigrationsIcon = () => {
+        if (plan.Migrations.length > 0) {
+          return <span className="pf-c-icon pf-m-info"><ServiceIcon /></span>;
+        } else {
+          return <span className="pf-c-icon"><ServiceIcon /></span>;
+        }
+      };
+      const parentIndex = planIndex * 2;
+      const planName = plan.MigPlan.metadata.name;
+      const planKey = `${planName}-${planIndex}`;
+
+      //check previous expanded value
+      let isOpenPrev = null;
+      if (currentRows.length > 0) {
+        const matchingIndex = currentRows.filter((_row, i) => i === parentIndex);
+        if (matchingIndex[0] && matchingIndex[0].cells.length > 0) {
+          isOpenPrev = matchingIndex[0].cells[1].props.isOpen;
+        }
+      }
+
+      const migStorageName = plan.MigPlan.spec.migStorageRef ?
+        plan.MigPlan.spec.migStorageRef.name : 'N/A';
+
+      const pvCount = plan.MigPlan.spec.persistentVolumes ?
+        plan.MigPlan.spec.persistentVolumes.length : 0;
+      return [
+        {
+          cells: [
+            {
+              title: (
+                <div className="pf-l-flex">
+                  <div className="pf-l-flex__item">
+                    <span>{plan.MigPlan.metadata.name}</span>
+                  </div>
+                </div>
+              ),
+
+              props: { component: 'th' },
+            },
+            {
+              title: (
+                <div className="pf-l-flex">
+                  <div className="pf-l-flex__item" key={planKey + '-icon'}>
+                    <MigrationsIcon />
+                  </div>
+                  <div className="pf-l-flex__item" key={planKey + '-text'}>
+                    <span>{plan.Migrations.length || 0}</span>
+                  </div>
+                </div>
+              ),
+
+              props: {
+                isOpen: isOpenPrev || false,
+                ariaControls: 'migrations-history-expansion-table',
+              },
+            },
+            {
+              title: <span>{plan.MigPlan.spec.srcMigClusterRef.name}</span>,
+            },
+            {
+              title: <span>{plan.MigPlan.spec.destMigClusterRef.name}</span>,
+            },
+            {
+              title: <span>{migStorageName}</span>,
+            },
+            {
+              title: (
+                <div className="pf-l-flex">
+                  <div className="pf-l-flex__item">
+                    <DatabaseIcon />
+                  </div>
+                  <div className="pf-l-flex__item">
+                    <span>{pvCount}</span>
+                  </div>
+                </div>
+              ),
+            },
+            {
+              title: <PlanStatus
+                plan={plan}
+              />,
+            },
+            {
+              title: <PlanActions
+                plan={plan}
+                toggleWizardOpen={toggleWizardOpen}
+              />,
+
+              props: {
+                className: 'pf-c-table__action',
+              }
+            },
+          ],
+        },
+        {
+          parent: parentIndex,
+          compoundParent: 1,
+          cells: [
+            {
+              title: (
+                <MigrationsTable
+                  type="Migrations"
+                  migrations={plan.Migrations}
+                  isPlanLocked={plan.PlanStatus.isPlanLocked}
+                  id="migrations-history-expansion-table"
+                />
+              ),
+              props: { colSpan: 9, className: 'pf-m-no-padding' },
+            },
+          ],
+        },
+      ];
+    });
+
+    return flatten(newRows);
+  };
   const [currentRows, setCurrentRows] = useState([]);
 
   useEffect(() => {
@@ -226,7 +227,7 @@ const PlanContent: React.FunctionComponent<IProps> = ({
         </DataList>
       ) : (
           <PlanEmptyState
-            toggleOpen={toggleOpen}
+            toggleOpen={toggleWizardOpen}
             plansDisabled={plansDisabled}
           />
         )}
