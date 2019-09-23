ac8aba50f36308d087130809ff4e5cdf33dd9eb5
Ian Bolton <ibolton@redhat.com>
2019-09-23 16:30:15 -0400

    wip convert table

--- src/app/plan/components/Wizard/VolumesTable.tsx
+++ src/app/plan/components/Wizard/VolumesTable.tsx
@@ -7,7 +7,12 @@ import { css } from '@emotion/core';
 import { Flex, Box, Text } from '@rebass/emotion';
 import styled from '@emotion/styled';
 import StatusIcon from '../../../common/components/StatusIcon';
-import { CubeIcon } from '@patternfly/react-icons';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  cellWidth
+} from '@patternfly/react-table';
 import {
   Bullseye,
   TextContent,
@@ -19,14 +24,6 @@ import {
   EmptyStateVariant,
   EmptyStateIcon,
   EmptyStateBody,
-  SelectDirection,
-  Dropdown,
-  DropdownToggle,
-  DropdownItem,
-  DropdownSeparator,
-  DropdownPosition,
-  DropdownDirection,
-  KebabToggle,
   Select,
   SelectOption,
   SelectVariant,
@@ -35,6 +32,7 @@ import theme from '../../../../theme';
 import ReactJson from 'react-json-view';
 import { BlueprintIcon, WarningTriangleIcon } from '@patternfly/react-icons';
 import { Spinner } from '@patternfly/react-core/dist/esm/experimental';
+import { isPlaceholder } from '@babel/types';
 
 const capitalize = (s: string) => {
   if (s.charAt(0)) {
@@ -62,47 +60,206 @@ const VolumesTable = (props): any => {
   const [isToggleIcon, setToggleIcon] = useState(false);
   const [selected, setSelected] = useState(null);
 
-  const onToggle = isOpen => {
-    setDropdownOpen(isOpen);
-  };
-  const onSelect = (row, selection) => {
-    setDropdownOpen(!isDropdownOpen);
-    updateTableData(row.index, selection);
-    setSelected(selection);
-  };
+  const columns = [
+    'PV Name',
+    'Project',
+    'Storage Class',
+    'Size',
+    'Claim',
+    'Type',
+    'Details'
+  ];
+  // columns={[
+  //   {
+  //     Header: () => (
+  //       <div
+  //         style={{
+  //           textAlign: 'left',
+  //           fontWeight: 600,
+  //         }}
+  //       >
+  //         PV Name
+  //       </div>
+  //     ),
+  //     accessor: 'name',
+  //     width: 180,
+  //   },
+  //   {
+  //     Header: () => (
+  //       <div
+  //         style={{
+  //           textAlign: 'left',
+  //           fontWeight: 600,
+  //         }}
+  //       >
+  //         Project
+  //       </div>
+  //     ),
+  //     accessor: 'project',
+  //     width: 150,
+  //   },
+  //   {
+  //     Header: () => (
+  //       <div
+  //         style={{
+  //           textAlign: 'left',
+  //           fontWeight: 600,
+  //         }}
+  //       >
+  //         Storage Class
+  //       </div>
+  //     ),
+  //     accessor: 'storageClass',
+  //     width: 150,
+  //   },
+  //   {
+  //     Header: () => (
+  //       <div
+  //         style={{
+  //           textAlign: 'left',
+  //           fontWeight: 600,
+  //         }}
+  //       >
+  //         Size
+  //       </div>
+  //     ),
+  //     accessor: 'size',
+  //     width: 75,
+  //   },
+  //   {
+  //     Header: () => (
+  //       <div
+  //         style={{
+  //           textAlign: 'left',
+  //           fontWeight: 600,
+  //         }}
+  //       >
+  //         Claim
+  //       </div>
+  //     ),
+  //     accessor: 'claim',
+  //     width: 180,
+  //   },
+  //   {
+  //     Header: () => (
+  //       <div
+  //         style={{
+  //           textAlign: 'left',
+  //           fontWeight: 600,
+  //         }}
+  //       >
+  //         Type
+  //       </div>
+  //     ),
+  //     accessor: 'type',
+  //     width: 120,
+  //     style: { overflow: 'visible' },
+  //     Cell: row => (
+  //       <Select
+  //         selections={selected}
+  //         isExpanded={isDropdownOpen}
+  //         onToggle={onToggle}
+  //         onSelect={(e, selection) => {
+  //           console.log('on select', e)
+  //           onSelect(e, row, selection)
+  //         }}
+  //       >
+  //         {row.original.supportedActions.map((action, index) => {
+  //           // console.log('supported Actions', row.original.supportedActions, action)
+  //           return (
+  //             <SelectOption
+  //               key={index}
+  //               value={action.value}
+  //             >
+  //               {/* {capitalize(action.value)} */}
+  //             </SelectOption>
+  //           )
+  //         })}
+  //       </Select>
+  //     ),
+  //   },
+  //   {
+  //     Header: () => (
+  //       <div
+  //         style={{
+  //           textAlign: 'left',
+  //           fontWeight: 600,
+  //         }}
+  //       >
+  //         Details
+  //       </div>
+  //     ),
+  //     accessor: 'details',
+  //     width: 200,
+  //     resizable: false,
+  //     textAlign: 'left',
+  //     Cell: row => {
+  //       const matchingPVResource = pvResourceList.find(
+  //         pvResource => pvResource.metadata.name === row.original.name
+  //       );
+  //       return (
+  //         <Popover
+  //           css={css`
+  //                 overflow-y: scroll;
+  //                 max-height: 20rem;
+  //                 width: 40rem;
+  //           `}
+  //           position={PopoverPosition.bottom}
+  //           bodyContent={
+  //             <React.Fragment>
+  //               {matchingPVResource ?
+  //                 <ReactJson src={matchingPVResource} enableClipboard={false} /> :
+  //                 <EmptyState variant={EmptyStateVariant.small}>
+  //                   <EmptyStateIcon icon={WarningTriangleIcon} />
+  //                   <Title headingLevel="h5" size="sm">
+  //                     No PV data found
+  //                 </Title>
+  //                   <EmptyStateBody>
+  //                     Unable to retrieve PV data
+  //                 </EmptyStateBody>
+  //                 </EmptyState>
+  //               }
+  //             </React.Fragment>
+  //           }
+  //           aria-label="pv-details"
+  //           closeBtnAriaLabel="close-pv-details"
+  //           maxWidth="200rem"
+  //         >
+  //           <Flex>
+  //             <Box>
+  //               <Button isDisabled={isFetchingPVResources} variant="link" icon={<BlueprintIcon />}>
+  //                 View JSON
+  //               </Button>
+  //             </Box>
+  //           </Flex>
+  //         </Popover>
+  //       );
+  //     },
+  //   },
+  // ]}
 
-  // const onSelect = (selection, isPlaceholder) => {
-  //   if (isPlaceholder) this.clearSelection();
-  //   else {
-  //     this.setState({
-  //       selected: selection,
-  //       isExpanded: false
-  //     });
-  //     console.log('selected:', selection);
+  // const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
+
+  // const updateTableData = (rowIndex, updatedValue) => {
+  //   const rowsCopy = [...rows];
+  //   if (currentPlan !== null && values.persistentVolumes) {
+  //     const updatedRow = { ...rowsCopy[rowIndex], type: updatedValue };
+
+  //     rowsCopy[rowIndex] = updatedRow;
   //   }
+
+  //   setRows(rowsCopy);
+  //   setFieldValue('persistentVolumes', rowsCopy);
   // };
 
-  const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
 
-  const updateTableData = (rowIndex, updatedValue) => {
-    const rowsCopy = [...rows];
-    if (currentPlan !== null && values.persistentVolumes) {
-      const updatedRow = { ...rowsCopy[rowIndex], type: updatedValue };
-
-      rowsCopy[rowIndex] = updatedRow;
-    }
-
-    setRows(rowsCopy);
-    setFieldValue('persistentVolumes', rowsCopy);
-  };
-
-  const getPVResources = (pvList = [], clusterName = '') => {
-    getPVResourcesRequest(pvList, clusterName);
-  };
 
   useEffect(() => {
     if (currentPlan) {
       const discoveredPersistentVolumes = currentPlan.spec.persistentVolumes || [];
+      const getPVResources = (pvList = [], clusterName = '') => {
+        getPVResourcesRequest(pvList, clusterName);
+      };
       getPVResources(discoveredPersistentVolumes, values.sourceCluster);
       let mappedPVs;
       if (values.persistentVolumes) {
@@ -117,30 +274,71 @@ const VolumesTable = (props): any => {
               pvAction = rowVal.type;
             }
           }
+          const pfSelectOptionsWithPlaceholder = planVolume.supported.actions.map((action, index) => {
+            return { value: action }
+          });
+          pfSelectOptionsWithPlaceholder.push({ value: 'Choose action...', isPlaceholder: true })
+
+          const rowCells = [
+            { title: planVolume.name },
+            { title: planVolume.pvc.namespace },
+            { title: planVolume.selection.storageClass || 'None' },
+            { title: planVolume.capacity },
+            { title: planVolume.pvc.name },
+            {
+              title: (
+                <Select
+                  selections={selected}
+                  isExpanded={isDropdownOpen}
+                  onToggle={onToggle}
+                  onSelect={(e, selection) => {
+                    console.log('on select', e)
+                    onSelect(e, selection, isPlaceholder)
+                  }}
+                >
+                  {pfSelectOptionsWithPlaceholder.map((action, index) => {
+                    // console.log('supported Actions', row.original.supportedActions, action)
+                    return (
+                      <SelectOption
+                        key={index}
+                        value={action.value}
+                      >
+                        {/* {capitalize(action.value)} */}
+                      </SelectOption>
+                    )
+                  })}
+                </Select>
+
+              )
+            },
+            { title: '' }
+            // { title: pfSelectOptionsWithPlaceholder }
+          ]
           return {
-            name: planVolume.name,
-            project: planVolume.pvc.namespace,
-            storageClass: planVolume.selection.storageClass || 'None',
-            size: planVolume.capacity,
-            claim: planVolume.pvc.name,
-            type: pvAction,
-            details: '',
-            supportedActions: planVolume.supported.actions,
-          };
+            cells: rowCells
+          }
         });
       } else {
         mappedPVs = discoveredPersistentVolumes.map(planVolume => {
           const pvAction = 'copy'; // Default to copy
+          const pfSelectOptionsWithPlaceholder = planVolume.supported.actions.map((action, index) => {
+            return { value: action }
+          });
+          pfSelectOptionsWithPlaceholder.push({ value: 'Choose action...', isPlaceholder: true })
+          const rowCells = [
+            { title: planVolume.name },
+            { title: planVolume.pvc.namespace },
+            { title: planVolume.selection.storageClass || '' },
+            { title: planVolume.capacity },
+            { title: planVolume.pvc.name },
+            { title: pvAction },
+            { title: '' }
+            // { pfSelectOptionsWithPlaceholder }
+          ]
           return {
-            name: planVolume.name,
-            project: planVolume.pvc.namespace,
-            storageClass: planVolume.selection.storageClass || '',
-            size: planVolume.capacity,
-            claim: planVolume.pvc.name,
-            type: pvAction,
-            details: '',
-            supportedActions: planVolume.supported.actions,
-          };
+            cells: rowCells
+          }
+
         });
       }
       setFieldValue('persistentVolumes', mappedPVs);
@@ -189,186 +387,45 @@ const VolumesTable = (props): any => {
     );
   }
 
+
+  // const onSelect = (e, row, selection) => {
+  //   setDropdownOpen(!isDropdownOpen);
+  //   updateTableData(row.index, selection);
+  //   setSelected(selection);
+  // };
+
+  const onToggle = isOpen => {
+    console.log('toggling', isOpen)
+    setDropdownOpen(isOpen);
+  };
+
+  const clearSelection = () => {
+    setDropdownOpen(false);
+    setSelected(null);
+  };
+
+  const onSelect = (event, selection, isPlaceholder) => {
+    console.log('selection', event, selection, isPlaceholder)
+    if (isPlaceholder) {
+      clearSelection();
+    } else {
+      setDropdownOpen(!isDropdownOpen);
+      // updateTableData(row.index, selection);
+      setSelected(selection);
+    }
+  }
+
+
   return (
-    <ReactTable
-      css={css`
-            font-size: 14px;
-            .rt-td{
-              margin: auto 0;
-            }
-      `}
-      data={rows}
-      columns={[
-        {
-          Header: () => (
-            <div
-              style={{
-                textAlign: 'left',
-                fontWeight: 600,
-              }}
-            >
-              PV Name
-            </div>
-          ),
-          accessor: 'name',
-          width: 180,
-        },
-        {
-          Header: () => (
-            <div
-              style={{
-                textAlign: 'left',
-                fontWeight: 600,
-              }}
-            >
-              Project
-            </div>
-          ),
-          accessor: 'project',
-          width: 150,
-        },
-        {
-          Header: () => (
-            <div
-              style={{
-                textAlign: 'left',
-                fontWeight: 600,
-              }}
-            >
-              Storage Class
-            </div>
-          ),
-          accessor: 'storageClass',
-          width: 150,
-        },
-        {
-          Header: () => (
-            <div
-              style={{
-                textAlign: 'left',
-                fontWeight: 600,
-              }}
-            >
-              Size
-            </div>
-          ),
-          accessor: 'size',
-          width: 75,
-        },
-        {
-          Header: () => (
-            <div
-              style={{
-                textAlign: 'left',
-                fontWeight: 600,
-              }}
-            >
-              Claim
-            </div>
-          ),
-          accessor: 'claim',
-          width: 180,
-        },
-        {
-          Header: () => (
-            <div
-              style={{
-                textAlign: 'left',
-                fontWeight: 600,
-              }}
-            >
-              Type
-            </div>
-          ),
-          accessor: 'type',
-          width: 120,
-          style: { overflow: 'visible' },
-          Cell: row => (
-            <Select
-              toggleIcon={isToggleIcon && <CubeIcon />}
-              variant={SelectVariant.single}
-              aria-label="Select Input"
-              onToggle={onToggle}
-              onSelect={(selection) => onSelect(row, selection)}
-              selections={selected}
-              isExpanded={isDropdownOpen}
-              ariaLabelledBy="action-select"
-              direction={SelectDirection.down}
-              name="persistentVolumes"
-            >
-              {row.original.supportedActions.map((a: string, index) => (
-                <SelectOption
-                  key={index}
-                  value={a}
-                >
-                  {capitalize(a)}
-                </SelectOption>
-              ))}
-            </Select>
-          ),
-        },
-        {
-          Header: () => (
-            <div
-              style={{
-                textAlign: 'left',
-                fontWeight: 600,
-              }}
-            >
-              Details
-            </div>
-          ),
-          accessor: 'details',
-          width: 200,
-          resizable: false,
-          textAlign: 'left',
-          Cell: row => {
-            const matchingPVResource = pvResourceList.find(
-              pvResource => pvResource.metadata.name === row.original.name
-            );
-            return (
-              <Popover
-                css={css`
-                      overflow-y: scroll;
-                      max-height: 20rem;
-                      width: 40rem;
-                `}
-                position={PopoverPosition.bottom}
-                bodyContent={
-                  <React.Fragment>
-                    {matchingPVResource ?
-                      <ReactJson src={matchingPVResource} enableClipboard={false} /> :
-                      <EmptyState variant={EmptyStateVariant.small}>
-                        <EmptyStateIcon icon={WarningTriangleIcon} />
-                        <Title headingLevel="h5" size="sm">
-                          No PV data found
-                      </Title>
-                        <EmptyStateBody>
-                          Unable to retrieve PV data
-                      </EmptyStateBody>
-                      </EmptyState>
-                    }
-                  </React.Fragment>
-                }
-                aria-label="pv-details"
-                closeBtnAriaLabel="close-pv-details"
-                maxWidth="200rem"
-              >
-                <Flex>
-                  <Box>
-                    <Button isDisabled={isFetchingPVResources} variant="link" icon={<BlueprintIcon />}>
-                      View JSON
-                    </Button>
-                  </Box>
-                </Flex>
-              </Popover>
-            );
-          },
-        },
-      ]}
-      defaultPageSize={5}
-      className="-striped -highlight"
-    />
+    <Table
+      aria-label="migrations-history-table"
+      cells={columns}
+      rows={rows}
+      className="pf-m-vertical-align-content-center"
+    >
+      <TableHeader />
+      <TableBody />
+    </Table>
   );
 };
 
