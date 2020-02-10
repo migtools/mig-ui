f9c705c9d9e6acc41eeacbeb67dbb7f8ed2f2762
Ian Bolton <ibolton@redhat.com>
2020-02-10 14:39:22 -0500

    convert wrapper component

---
+++ src/app/common/components/HideWrapper.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+const styles = require('./HideWrapper.module')
+
+const HideWrapper = (props) => (
+  <div {...props} className={styles.hideWrapper}>{props.children}</div>
+)
+
+export default HideWrapper;
