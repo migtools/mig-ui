551eed1b9160f8176147f66375e790d590350388
Ian Bolton <ibolton@redhat.com>
2020-02-11 10:54:05 -0500

    remove rebass for storage forms

--- src/app/logs/LogsComponent.tsx
+++ src/app/logs/LogsComponent.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { Flex, Box } from '@rebass/emotion';
 import {
   Page,
   PageSection,
+  Grid,
+  GridItem
 } from '@patternfly/react-core';
 import HeaderComponent from '../common/components/HeaderComponent';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbHeading } from '@patternfly/react-core';
@@ -18,16 +18,16 @@ const LogsComponent: React.FunctionComponent<IProps> = ({ match }) => {
   return (
     <Page header={HeaderComponent}>
       <PageSection>
-        <Flex justifyContent="center">
-          <Box flex="0 0 100%">
+        <Grid gutter="md">
+          <GridItem>
             <Breadcrumb>
               <BreadcrumbItem>
                 <Link to="/">Home</Link>
               </BreadcrumbItem>
               <BreadcrumbItem to="#" isActive>{match.params.planId} Logs</BreadcrumbItem>
             </Breadcrumb>
-          </Box>
-        </Flex>
+          </GridItem>
+        </Grid>
       </PageSection>
       <PageSection>
         <LogsContainer planName={match.params.planId} />
