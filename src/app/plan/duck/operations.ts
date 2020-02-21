08d6f08fb52bafc0db3bfd9768a18f6f9c94c476
Ian Bolton <ibolton@redhat.com>
2020-02-21 14:11:49 -0500

    wip

---
+++ src/app/plan/duck/operations.ts
@@ -0,0 +1,241 @@
+import { PlanActions } from './actions';
+import { ClientFactory } from '../../../client/client_factory';
+import { IClusterClient } from '../../../client/client';
+import { MigResource, MigResourceKind } from '../../../client/resources';
+
+import {
+  createMigMigration,
+  createInitialMigPlan,
+} from '../../../client/resources/conversions';
+import {
+  AlertActions
+} from '../../common/duck/actions';
+import planUtils from './utils';
+import { select, put } from 'redux-saga/effects';
+import { PollingActions } from '../../common/duck/actions';
+import { IDiscoveryClient } from '../../../client/discoveryClient';
+import { NamespaceDiscovery } from '../../../client/resources/discovery';
+import { DiscoveryResource } from '../../../client/resources/common';
+import utils from '../../common/duck/utils';
+
+const uuidv1 = require('uuid/v1');
+const PlanMigrationPollingInterval = 5000;
+
+function fetchMigMigrationsRefs(client: IClusterClient, migMeta, migPlans): Array<Promise<any>> {
+  const refs: Array<Promise<any>> = [];
+
+  migPlans.forEach(plan => {
+    const migMigrationResource = new MigResource(MigResourceKind.MigMigration, migMeta.namespace);
+    refs.push(client.list(migMigrationResource));
+  });
+
+  return refs;
+}
+
+function* fetchPlansGenerator() {
+  const state = yield select();
+  const client: IClusterClient = ClientFactory.cluster(state);
+  const resource = new MigResource(MigResourceKind.MigPlan, state.migMeta.namespace);
+  try {
+    let planList = yield client.list(resource);
+    planList = yield planList.data.items;
+    const refs = yield Promise.all(fetchMigMigrationsRefs(client, state.migMeta, planList));
+    const groupedPlans = yield planUtils.groupPlans(planList, refs);
+    return { updatedPlans: groupedPlans, isSuccessful: true };
+  } catch (e) {
+    put(AlertActions.alertErrorTimeout('Failed to fetch migration plans. '));
+
+    return { e, isSuccessful: false };
+  }
+}
+
+const runStage = plan => {
+  return async (dispatch, getState) => {
+    try {
+      dispatch(PlanActions.initStage(plan.MigPlan.metadata.name));
+      dispatch(AlertActions.alertProgressTimeout('Staging Started'));
+      const { migMeta } = getState();
+      const client: IClusterClient = ClientFactory.cluster(getState());
+      const migMigrationObj = createMigMigration(
+        uuidv1(),
+        plan.MigPlan.metadata.name,
+        migMeta.namespace,
+        true,
+        true
+      );
+      const migMigrationResource = new MigResource(MigResourceKind.MigMigration, migMeta.namespace);
+
+      //created migration response object
+      const createMigRes = await client.create(migMigrationResource, migMigrationObj);
+      const migrationListResponse = await client.list(migMigrationResource);
+      const groupedPlan = planUtils.groupPlan(plan, migrationListResponse);
+
+      const getStageStatusCondition = (pollingResponse, newObjectRes) => {
+        const matchingPlan = pollingResponse.updatedPlans.find(
+          p => p.MigPlan.metadata.name === newObjectRes.data.spec.migPlanRef.name
+        );
+
+        const migStatus = matchingPlan
+          ? planUtils.getMigrationStatus(matchingPlan, newObjectRes)
+          : null;
+        if (migStatus.success) {
+          dispatch(PlanActions.stagingSuccess(newObjectRes.data.spec.migPlanRef.name));
+          dispatch(AlertActions.alertSuccessTimeout('Staging Successful'));
+          return 'SUCCESS';
+        } else if (migStatus.error) {
+          dispatch(PlanActions.stagingFailure(migStatus.error));
+          dispatch(AlertActions.alertErrorTimeout('Staging Failed'));
+          return 'FAILURE';
+        }
+      };
+
+      const params = {
+        asyncFetch: fetchPlansGenerator,
+        delay: PlanMigrationPollingInterval,
+        callback: getStageStatusCondition,
+        type: 'STAGE',
+        statusItem: createMigRes,
+        dispatch,
+      };
+
+      dispatch(PollingActions.startStatusPolling(params));
+      dispatch(PlanActions.updatePlanMigrations(groupedPlan));
+    } catch (err) {
+      dispatch(AlertActions.alertErrorTimeout(err));
+      dispatch(PlanActions.stagingFailure(err));
+    }
+  };
+};
+
+const runMigration = (plan, disableQuiesce) => {
+  return async (dispatch, getState) => {
+    try {
+      dispatch(PlanActions.initMigration(plan.MigPlan.metadata.name));
+      dispatch(AlertActions.alertProgressTimeout('Migration Started'));
+      const { migMeta } = getState();
+      const client: IClusterClient = ClientFactory.cluster(getState());
+
+      const migMigrationObj = createMigMigration(
+        uuidv1(),
+        plan.MigPlan.metadata.name,
+        migMeta.namespace,
+        false,
+        disableQuiesce
+      );
+      const migMigrationResource = new MigResource(MigResourceKind.MigMigration, migMeta.namespace);
+
+      //created migration response object
+      const createMigRes = await client.create(migMigrationResource, migMigrationObj);
+
+      const migrationListResponse = await client.list(migMigrationResource);
+      const groupedPlan = planUtils.groupPlan(plan, migrationListResponse);
+
+      const getMigrationStatusCondition = (pollingResponse, newObjectRes) => {
+        const matchingPlan = pollingResponse.updatedPlans.find(
+          p => p.MigPlan.metadata.name === newObjectRes.data.spec.migPlanRef.name
+        );
+        const migStatus = matchingPlan
+          ? planUtils.getMigrationStatus(matchingPlan, newObjectRes)
+          : null;
+        if (migStatus.success) {
+          dispatch(PlanActions.migrationSuccess(newObjectRes.data.spec.migPlanRef.name));
+          dispatch(PlanActions.planCloseRequest(newObjectRes.data.spec.migPlanRef.name));
+          dispatch(AlertActions.alertSuccessTimeout('Migration Successful'));
+          return 'SUCCESS';
+        } else if (migStatus.error) {
+          dispatch(PlanActions.migrationFailure(migStatus.error));
+          dispatch(AlertActions.alertErrorTimeout('Migration Failed'));
+          return 'FAILURE';
+        }
+      };
+
+      const params = {
+        asyncFetch: fetchPlansGenerator,
+        delay: PlanMigrationPollingInterval,
+        callback: getMigrationStatusCondition,
+        type: 'MIGRATION',
+        statusItem: createMigRes,
+        dispatch,
+      };
+
+      dispatch(PollingActions.startStatusPolling(params));
+      dispatch(PlanActions.updatePlanMigrations(groupedPlan));
+    } catch (err) {
+      dispatch(AlertActions.alertErrorTimeout(err));
+      dispatch(PlanActions.migrationFailure(err));
+    }
+  };
+};
+
+const addPlan = (migPlan) => {
+  return async (dispatch, getState) => {
+    try {
+      /**
+       * Create the plan object. Blocks all code in this function until createPlanRes
+       */
+      const { migMeta } = getState();
+      const client: IClusterClient = ClientFactory.cluster(getState());
+
+      const migPlanObj = createInitialMigPlan(
+        migPlan.planName,
+        migMeta.namespace,
+        migPlan.sourceCluster,
+        migPlan.targetCluster,
+        migPlan.selectedStorage,
+        migPlan.namespaces
+      );
+
+      const createPlanRes = await client.create(
+        new MigResource(MigResourceKind.MigPlan, migMeta.namespace),
+        migPlanObj
+      );
+
+      dispatch(PlanActions.setCurrentPlan(createPlanRes.data));
+      dispatch(PlanActions.addPlanSuccess(createPlanRes.data));
+
+    } catch (err) {
+      dispatch(AlertActions.alertErrorTimeout('Failed to add plan'));
+    }
+  };
+};
+
+const removePlan = id => {
+  throw new Error('NOT IMPLEMENTED');
+};
+
+
+const fetchPlans = () => {
+  return async (dispatch, getState) => {
+    dispatch(PlanActions.migPlanFetchRequest());
+    try {
+      const { migMeta } = getState();
+      const client: IClusterClient = ClientFactory.cluster(getState());
+      const resource = new MigResource(MigResourceKind.MigPlan, migMeta.namespace);
+      const res = await client.list(resource);
+      const migPlans = res.data.items || [];
+
+      const refs = await Promise.all(fetchMigMigrationsRefs(client, migMeta, migPlans));
+      const groupedPlans = planUtils.groupPlans(migPlans, refs);
+      dispatch(PlanActions.migPlanFetchSuccess(groupedPlans));
+    } catch (err) {
+      if (err.response) {
+        dispatch(AlertActions.alertErrorTimeout(err.response.data.message));
+      } else if (err.message) {
+        dispatch(AlertActions.alertErrorTimeout(err.message));
+      } else {
+        dispatch(AlertActions.alertErrorTimeout('Failed to fetch plans: An unknown error occurred'));
+      }
+      dispatch(PlanActions.migPlanFetchFailure());
+    }
+  };
+};
+
+export default {
+  fetchPlans,
+  addPlan,
+  removePlan,
+  runStage,
+  runMigration,
+  fetchMigMigrationsRefs,
+  fetchPlansGenerator,
+};
---
+++ src/app/storage/duck/operations.ts
@@ -0,0 +1,93 @@
+import { StorageActions } from './actions';
+import { ClientFactory } from '../../../client/client_factory';
+import { IClusterClient } from '../../../client/client';
+import { MigResource, MigResourceKind } from '../../../client/resources';
+import { select, put } from 'redux-saga/effects';
+
+import { CoreNamespacedResource, CoreNamespacedResourceKind } from '../../../client/resources';
+
+import {
+  AlertActions
+} from '../../common/duck/actions';
+
+const updateSearchTerm = StorageActions.updateSearchTerm;
+
+const removeStorage = name => {
+  return async (dispatch, getState) => {
+    try {
+      const state = getState();
+      const { migMeta } = state;
+      const client: IClusterClient = ClientFactory.cluster(state);
+
+      const secretResource = new CoreNamespacedResource(
+        CoreNamespacedResourceKind.Secret,
+        migMeta.namespace
+      );
+      const migStorageResource = new MigResource(MigResourceKind.MigStorage, migMeta.namespace);
+
+      const arr = await Promise.all([
+        client.delete(secretResource, name),
+        client.delete(migStorageResource, name),
+      ]);
+
+      dispatch(StorageActions.removeStorageSuccess(name));
+      dispatch(AlertActions.alertSuccessTimeout(`Successfully removed replication repository "${name}"!`));
+    } catch (err) {
+      dispatch(AlertActions.alertErrorTimeout(err));
+    }
+  };
+};
+
+function fetchMigStorageRefs(client: IClusterClient, migMeta, migStorages): Array<Promise<any>> {
+  const refs: Array<Promise<any>> = [];
+
+  migStorages.forEach(storage => {
+    const secretRef = storage.spec.backupStorageConfig.credsSecretRef;
+    const secretResource = new CoreNamespacedResource(
+      CoreNamespacedResourceKind.Secret,
+      secretRef.namespace
+    );
+    refs.push(client.get(secretResource, secretRef.name));
+  });
+
+  return refs;
+}
+
+function groupStorages(migStorages: any[], refs: any[]): any[] {
+  return migStorages.map(ms => {
+    const fullStorage = {
+      MigStorage: ms,
+    };
+    // TODO: When VSL configuration is supported separate from BSL,
+    // this needs to be updated to support two different, distinct secrets
+    fullStorage['Secret'] = refs.find(
+      ref => ref.data.kind === 'Secret' &&
+        ref.data.metadata.name === ms.spec.backupStorageConfig.credsSecretRef.name
+    ).data;
+
+    return fullStorage;
+  });
+}
+
+function* fetchStorageGenerator() {
+  const state = yield select();
+  const client: IClusterClient = ClientFactory.cluster(state);
+  const resource = new MigResource(MigResourceKind.MigStorage, state.migMeta.namespace);
+  try {
+    let storageList = yield client.list(resource);
+    storageList = yield storageList.data.items;
+    const refs = yield Promise.all(fetchMigStorageRefs(client, state.migMeta, storageList));
+    const groupedStorages = groupStorages(storageList, refs);
+    return { updatedStorages: groupedStorages, isSuccessful: true };
+  } catch (e) {
+    put(AlertActions.alertErrorTimeout('Failed to fetch replication repositories. '));
+
+    return { e, isSuccessful: false };
+  }
+}
+
+export default {
+  fetchStorageGenerator,
+  removeStorage,
+  updateSearchTerm,
+};
