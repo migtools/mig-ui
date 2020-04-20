74e7f407c5b71c30b9352bd45c09b046faf9488f
Gilles Dubreuil <gilles@redhat.com>
2020-04-20 17:12:16 +0200

    Removed eslint react plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,6 @@ module.exports = {
     extends: [
       'eslint:recommended',
       'plugin:@typescript-eslint/eslint-recommended',
-      /* Enforcing react plugin
-      'plugin:react/recommended',
-      */
       'plugin:@typescript-eslint/recommended',
       'prettier/@typescript-eslint',
       'plugin:prettier/recommended',
