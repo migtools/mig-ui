033b2c7ed6234de3f10897d1ed155aec0f6508f5
Gilles Dubreuil <gilles@redhat.com>
2020-04-20 15:58:42 +0200

    Links prettier with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,40 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-    ],
+    plugins: ['@typescript-eslint'],
     extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      /* Enforcing react plugin
+      'plugin:react/recommended',
+      */
+      'plugin:@typescript-eslint/recommended',
+      'prettier/@typescript-eslint',
+      'plugin:prettier/recommended',
     ],
     rules: {
-        "indent": ["error", 2, { "SwitchCase": 1 }],
-        "keyword-spacing": 2,
-        "switch-colon-spacing": ["error", { "after": true, "before": false }],
-        "@typescript-eslint/interface-name-prefix": "off",
-        "@typescript-eslint/ban-ts-ignore": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "no-case-declarations": "off",
-        "no-var-requires": "off",
-        "@typescript-eslint/no-var-requires": "off",
-        "@typescript-eslint/camelcase": "off",
-        "no-prototype-builtins": "off",
-        "no-useless-escape": "off",
-        "no-useless-catch": "off"
-    }
-}
+      'keyword-spacing': 2,
+      'switch-colon-spacing': ['error', { after: true, before: false }],
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/ban-ts-ignore': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-case-declarations': 'off',
+      'no-var-requires': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/camelcase': 'off',
+      'no-prototype-builtins': 'off',
+      'no-useless-escape': 'off',
+      'no-useless-catch': 'off',
+    },
+    parserOptions: {
+      ecmaVersion: 2016,
+      sourceType: 'module',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  };
