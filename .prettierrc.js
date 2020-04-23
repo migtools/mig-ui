ce76d7cc85fd047cc05b90f5450fb2760308813b
Gilles Dubreuil <gilles@redhat.com>
2020-04-23 23:13:42 +0200

    Yarn scripts: Add prettier:fix rule + prettier check to lint rule (#831)
    * Yarn scripts: Add prettier:fix rule + prettier check to lint rule
    
    * Change yarn/prettier:fix to do all files but yaml; lint to run prettier
    
    * Remove archive/tlsint.json
    
    Co-authored-by: Erik Nelson <erik@nsk.io>

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    bracketSpacing: true,
-    printWidth: 100,
-    semi: true,
-    singleQuote: true,
-    tabWidth: 2,
-    trailingComma: "es5",
-  };
+  bracketSpacing: true,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+};
