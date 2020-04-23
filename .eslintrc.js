cde65d3b02b0d279c8ae985e8707ae3eb0cfef8a
Gilles Dubreuil <gilles@redhat.com>
2020-04-23 14:50:48 +0200

    eslintrc to get js indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,37 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/eslint-recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint',
-      'plugin:prettier/recommended',
-    ],
-    rules: {
-      'keyword-spacing': 2,
-      'switch-colon-spacing': ['error', { after: true, before: false }],
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/ban-ts-ignore': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      'no-case-declarations': 'off',
-      'no-var-requires': 'off',
-      '@typescript-eslint/no-var-requires': 'off',
-      '@typescript-eslint/camelcase': 'off',
-      'no-prototype-builtins': 'off',
-      'no-useless-escape': 'off',
-      'no-useless-catch': 'off',
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  rules: {
+    'keyword-spacing': 2,
+    'switch-colon-spacing': ['error', { after: true, before: false }],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-case-declarations': 'off',
+    'no-var-requires': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    'no-prototype-builtins': 'off',
+    'no-useless-escape': 'off',
+    'no-useless-catch': 'off',
+  },
+  parserOptions: {
+    ecmaVersion: 2016,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    parserOptions: {
-      ecmaVersion: 2016,
-      sourceType: 'module',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  };
+  },
+};
