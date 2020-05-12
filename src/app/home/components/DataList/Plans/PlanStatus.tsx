7548811b52dc96e57b1f86ebc7561f5b1b492c21
Ian Bolton <ibolton@redhat.com>
2020-05-12 14:18:35 -0400

    move conflict text to popover and add icon

--- src/app/home/components/DataList/Plans/PlanStatus.tsx
+++ src/app/home/components/DataList/Plans/PlanStatus.tsx
@@ -29,8 +29,7 @@ const PlanStatus: React.FunctionComponent<IProps> = ({ plan }) => {
     } else if (hasClosedCondition) {
       status.text = 'Closed';
     } else if (hasConflictCondition) {
-      status.text = `${conflictErrorMsg} 
-      Please check for conflicting namespaces or delete the plan(s).`;
+      status.text = conflictErrorMsg;
     } else if (hasRunningMigrations) {
       status.text = `${latestType} Running`;
     } else if (hasCanceledCondition) {
