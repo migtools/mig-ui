e19e366015533ee206329358d7de881886445bd1
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-06-05 16:52:39 -0400

    Try out an action handler map instead of switch statement for the reducer

--- src/app/token/duck/reducers.ts
+++ src/app/token/duck/reducers.ts
@@ -20,31 +20,26 @@ export const INITIAL_STATE: ITokenReducerState = {
 
 // NATODO what can we abstract out here? Take lessons from Migration Analytics?
 
-export const tokenFetchRequest: TokenReducerFn = (state = INITIAL_STATE, action) => {
-  return { ...state, isFetching: true };
-};
+// NATODO how do we like this structure? maybe refactor other reducers to use it?
+// One benefit is it allows us to abstract out common patterns for initial state and action handlers and spread them into this object.
+// See https://github.com/RedHatCloudForms/cfme-migration_analytics/blob/master/app/javascript/react/screens/App/Analytics/redux/analyticsReducer.js
 
-export const tokenFetchSuccess: TokenReducerFn = (state = INITIAL_STATE, action) => {
-  return { ...state, tokenList: action.tokenList, isFetching: false };
+const tokenActionHandlers: { [actionType: string]: TokenReducerFn } = {
+  [TokenActionTypes.MIG_TOKEN_FETCH_REQUEST]: (state = INITIAL_STATE, action) => {
+    return { ...state, isFetching: true };
+  },
+  [TokenActionTypes.MIG_TOKEN_FETCH_SUCCESS]: (state = INITIAL_STATE, action) => {
+    return { ...state, tokenList: action.tokenList, isFetching: false };
+  },
+  [TokenActionTypes.MIG_TOKEN_FETCH_FAILURE]: (state = INITIAL_STATE, action) => {
+    return { ...state, isError: true, isFetching: false };
+  },
+  // NATODO add handlers for the other actions
 };
-
-export const tokenFetchFailure: TokenReducerFn = (state = INITIAL_STATE, action) => {
-  return { ...state, isError: true, isFetching: false };
-};
-
-// NATODO etc, for the other actions
 
 export const tokenReducer: TokenReducerFn = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case TokenActionTypes.MIG_TOKEN_FETCH_REQUEST:
-      return tokenFetchRequest(state, action);
-    case TokenActionTypes.MIG_TOKEN_FETCH_SUCCESS:
-      return tokenFetchSuccess(state, action);
-    case TokenActionTypes.MIG_TOKEN_FETCH_FAILURE:
-      return tokenFetchFailure(state, action);
-    default:
-      return state;
-  }
+  const handler = tokenActionHandlers[action.type];
+  return handler ? handler(state, action) : state;
 };
 
 export default tokenReducer;
