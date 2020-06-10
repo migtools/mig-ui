b6bfce56fd553585b8f7f0f4d587768aa208514b
Gilles Dubreuil <gilles@redhat.com>
2020-06-10 11:37:21 +0200

    Add test checking component renders within Dom

--- src/app/plan/components/Wizard/__tests__/GeneralForm.test.tsx
+++ src/app/plan/components/Wizard/__tests__/GeneralForm.test.tsx
@@ -1,34 +1,51 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { render, cleanup, screen, fireEvent } from '@testing-library/react';
 import GeneralForm, { IGeneralFormProps } from '../GeneralForm';
 import '@testing-library/jest-dom';
 
 import { planValue } from './planHelper';
 
-function renderGeneralForm(props: Partial<IGeneralFormProps> = {}) {
-  const defaultProps: IGeneralFormProps = {
-    handleBlur() {
-      return;
-    },
-    handleChange() {
-      return;
-    },
-    setFieldTouched() {
-      return;
-    },
-    touched: {
-      planName: true,
-    },
-    errors: '',
-    isEdit: true,
-    values: planValue,
-  };
+const defaultProps: IGeneralFormProps = {
+  handleBlur() {
+    return;
+  },
+  handleChange() {
+    return;
+  },
+  setFieldTouched() {
+    return;
+  },
+  touched: {
+    planName: true,
+  },
+  errors: '',
+  isEdit: true,
+  values: planValue,
+};
 
+function renderGeneralForm(props: Partial<IGeneralFormProps> = {}) {
   return render(<GeneralForm {...defaultProps} {...props} />);
 }
 
+// afterEach(cleanup);
+
 describe('<GeneralForm />', () => {
-  test('has a plan name', async () => {
+  it('renders component', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<GeneralForm {...defaultProps}></GeneralForm>, div);
+  });
+
+  it('has a plan name', async () => {
+    renderGeneralForm();
+    const planForm = await screen.findByTestId('plan-form');
+
+    expect(planForm).toHaveFormValues({
+      planName: 'plan-name-test',
+    });
+  });
+
+  it('has a plan name', async () => {
     renderGeneralForm();
     const planForm = await screen.findByTestId('plan-form');
 
