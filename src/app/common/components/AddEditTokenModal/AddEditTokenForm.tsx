e1d1a55045c16241d3e6065ab0f998edf1e2fb72
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-06-10 17:46:47 -0400

    Add cluster (stub), token type fields, buttons

--- src/app/common/components/AddEditTokenModal/AddEditTokenForm.tsx
+++ src/app/common/components/AddEditTokenModal/AddEditTokenForm.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
-import { Form, FormGroup, TextInput } from '@patternfly/react-core';
+import {
+  Form,
+  FormGroup,
+  TextInput,
+  Radio,
+  TextContent,
+  Text,
+  Button,
+  Flex,
+  FlexModifiers,
+} from '@patternfly/react-core';
+import spacing from '@patternfly/react-styles/css/utilities/Spacing/spacing';
 import { FormikProps, withFormik, FormikBag } from 'formik';
-import { IAddEditStatus, AddEditMode } from '../../add_edit_state';
+import {
+  IAddEditStatus,
+  AddEditMode,
+  addEditButtonText,
+  isAddEditButtonDisabled,
+} from '../../add_edit_state';
+import SimpleSelect from '../SimpleSelect';
 
 enum FieldKey {
   name = 'name',
+  associatedClusterName = 'associatedClusterName',
+  tokenType = 'tokenType',
+  serviceAccountToken = 'serviceAccountToken',
+}
+
+enum TokenType {
+  oAuth = 'oAuth',
+  serviceAccount = 'serviceAccount',
 }
 
 interface ITokenFormValues {
   [FieldKey.name]: string;
+  [FieldKey.associatedClusterName]: string;
+  [FieldKey.tokenType]: TokenType;
+  [FieldKey.serviceAccountToken]: string;
 }
 
 interface IOtherProps {
@@ -34,6 +62,10 @@ const InnerAddEditTokenForm: React.FunctionComponent<
   const formikHandleChange = (_val, e) => handleChange(e);
   const formikSetFieldTouched = (key: FieldKey) => () => setFieldTouched(key, true, true);
 
+  const onLogInClick = () => {
+    alert('NATODO: not yet implemented');
+  };
+
   return (
     <Form onSubmit={handleSubmit}>
       <FormGroup
@@ -43,8 +75,6 @@ const InnerAddEditTokenForm: React.FunctionComponent<
         helperTextInvalid={touched.name && errors.name}
         isValid={!(touched.name && errors.name)}
       >
-        {/*
-          // @ts-ignore issue: https://github.com/konveyor/mig-ui/issues/747 */}
         <TextInput
           onChange={formikHandleChange}
           onInput={formikSetFieldTouched(FieldKey.name)}
@@ -52,11 +82,100 @@ const InnerAddEditTokenForm: React.FunctionComponent<
           value={values.name}
           name={FieldKey.name}
           type="text"
-          id="cluster-name-input"
+          id={FieldKey.name}
           isDisabled={addEditStatus.mode === AddEditMode.Edit}
           isValid={!(touched.name && errors.name)}
         />
       </FormGroup>
+      <FormGroup
+        label="Associate with cluster"
+        isRequired
+        fieldId={FieldKey.associatedClusterName}
+        helperTextInvalid={touched.associatedClusterName && errors.associatedClusterName}
+        isValid={!(touched.associatedClusterName && errors.associatedClusterName)}
+      >
+        <SimpleSelect
+          id={FieldKey.associatedClusterName}
+          onChange={(selection) => setFieldValue(FieldKey.associatedClusterName, selection)}
+          options={['foo', 'bar', 'baz']} // NATODO
+          value={values.associatedClusterName}
+          placeholderText="Select cluster..."
+        />
+      </FormGroup>
+      <FormGroup
+        label="Token type"
+        isRequired
+        fieldId={FieldKey.tokenType}
+        helperTextInvalid={touched.tokenType && errors.tokenType}
+        isValid={!(touched.tokenType && errors.tokenType)}
+      >
+        <Radio
+          id={`${FieldKey.tokenType}-${TokenType.oAuth}`}
+          name={FieldKey.tokenType}
+          isChecked={values.tokenType === TokenType.oAuth}
+          onChange={(checked) => {
+            if (checked) setFieldValue(FieldKey.tokenType, TokenType.oAuth);
+          }}
+          label="OAuth"
+        />
+        <div className={`${spacing.mtMd} ${spacing.mbLg} ${spacing.mlLg}`}>
+          <TextContent>
+            <Text component="p">
+              Log in to the cluster to automatically generate the token, or enter the token string
+              in the field.
+            </Text>
+          </TextContent>
+          <Button
+            variant="secondary"
+            className={spacing.mtSm}
+            onClick={onLogInClick}
+            isDisabled={values.tokenType !== TokenType.oAuth}
+          >
+            Log in
+          </Button>
+        </div>
+        <Radio
+          id={`${FieldKey.tokenType}-${TokenType.serviceAccount}`}
+          name={FieldKey.tokenType}
+          isChecked={values.tokenType === TokenType.oAuth}
+          onChange={(checked) => {
+            if (checked) setFieldValue(FieldKey.tokenType, TokenType.serviceAccount);
+          }}
+          label="Service account"
+        />
+        <FormGroup
+          className={`${spacing.mtMd} ${spacing.mbLg} ${spacing.mlLg}`}
+          fieldId={FieldKey.serviceAccountToken}
+          isRequired={values.tokenType === TokenType.serviceAccount}
+          helperText="Enter the token string."
+          helperTextInvalid={touched.serviceAccountToken && errors.serviceAccountToken}
+          isValid={!(touched.serviceAccountToken && errors.serviceAccountToken)}
+        >
+          <TextInput
+            onChange={formikHandleChange}
+            onInput={formikSetFieldTouched(FieldKey.serviceAccountToken)}
+            onBlur={handleBlur}
+            value={values.serviceAccountToken}
+            name={FieldKey.serviceAccountToken}
+            type="text"
+            id={FieldKey.serviceAccountToken}
+            isDisabled={values.tokenType !== TokenType.serviceAccount}
+            isValid={!(touched.serviceAccountToken && errors.serviceAccountToken)}
+          />
+        </FormGroup>
+      </FormGroup>
+      <Flex breakpointMods={[{ modifier: FlexModifiers['space-items-md'] }]}>
+        <Button
+          variant="primary"
+          type="submit"
+          isDisabled={isAddEditButtonDisabled(addEditStatus, errors, touched, true)}
+        >
+          {addEditButtonText('token')(addEditStatus)}
+        </Button>
+        <Button variant="secondary" onClick={onClose}>
+          Close
+        </Button>
+      </Flex>
     </Form>
   );
 };
@@ -65,6 +184,9 @@ const AddEditTokenForm = withFormik<IOtherProps, ITokenFormValues>({
   mapPropsToValues: () => ({
     // NATODO initialize here from existing token for editing?
     name: '',
+    associatedClusterName: '',
+    tokenType: TokenType.oAuth,
+    serviceAccountToken: '',
   }),
   validate: (values: ITokenFormValues) => ({}), // NATODO add validation logic
   handleSubmit: (values: ITokenFormValues, formikBag: FormikBag<IOtherProps, ITokenFormValues>) => {
