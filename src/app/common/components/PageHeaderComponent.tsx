69228ff42dc88c5807f1127a5901062153f56985
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-06-18 16:44:54 -0400

    Add WIP banner to masthead (#927)

--- src/app/common/components/PageHeaderComponent.tsx
+++ src/app/common/components/PageHeaderComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PageHeader, Brand, PageHeaderProps } from '@patternfly/react-core';
+import { PageHeader, Brand, PageHeaderProps, Title } from '@patternfly/react-core';
 import openshiftLogo from './CAM_LOGO.svg';
 const styles = require('./PageHeaderComponent.module');
 
@@ -7,7 +7,17 @@ type PageHeaderComponentProps = Omit<PageHeaderProps, 'logo'>;
 
 const PageHeaderComponent: React.FunctionComponent<PageHeaderComponentProps> = (props) => (
   <PageHeader
-    logo={<Brand className={styles.logoPointer} src={openshiftLogo} alt="OpenShift Logo" />}
+    logo={
+      <>
+        <Brand className={styles.logoPointer} src={openshiftLogo} alt="OpenShift Logo" />
+        <Title
+          size="2xl"
+          style={{ marginLeft: 40, backgroundColor: 'red', color: 'white', padding: 10 }}
+        >
+          WORK IN PROGRESS
+        </Title>
+      </>
+    }
     {...props}
   />
 );
