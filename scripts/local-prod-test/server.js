33917ecf144cc4789f2fd0c9f488dae8c6ebcc3e
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-07-01 17:00:31 -0400

    Fix existing localprod express route match

--- scripts/local-prod-test/server.js
+++ scripts/local-prod-test/server.js
@@ -5,7 +5,7 @@ const port = 9000
 
 app.use('/', express.static(path.join(__dirname + '../../..' + '/dist')))
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname + '../../..' + '/dist/index.html'));
 });
 app.listen(port, () => console.log(`Local production emulation listening on port ${port}!`))
