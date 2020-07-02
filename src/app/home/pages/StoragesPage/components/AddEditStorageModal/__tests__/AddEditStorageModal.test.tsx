80e15308c698e04574587072469307e60efbb6e2
Gilles Dubreuil <gilles@redhat.com>
2020-07-02 04:41:11 +0200

    Add test for S3 storage (#950)
    * Add test for S3 storage
    
    * Better test name
    
    Co-authored-by: Ian Bolton <ibolton@redhat.com>

---
+++ src/app/home/pages/StoragesPage/components/AddEditStorageModal/__tests__/AddEditStorageModal.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from '../../../../../../../reducers';
+
+import AddEditStorageModal from '../index';
+
+const store = createStore(rootReducer, {});
+
+describe('<AddEditStorageModal />', () => {
+  it('allows filling a S3 form with valid values', () => {
+    render(
+      <Provider store={store}>
+        <AddEditStorageModal isOpen={true} />
+      </Provider>
+    );
+
+    userEvent.click(screen.getByText('Select a type...'));
+    userEvent.click(screen.getByText('S3'));
+    userEvent.type(screen.getByTestId('storage-name-input'), 'storage-name');
+    userEvent.type(screen.getByTestId('storage-bucket-name-input'), 'storage-bucket-name');
+    userEvent.type(screen.getByTestId('storage-bucket-region-input'), 'storage-bucket-region');
+    userEvent.type(screen.getByTestId('storage-s3-url-input'), 'storage-s3-url');
+    userEvent.type(screen.getByTestId('storage-access-key-input'), 'storage-s3-user');
+    userEvent.type(screen.getByTestId('storage-secret-input'), 'storage-s3-password');
+
+    expect(screen.getByDisplayValue('storage-name')).toBeInTheDocument;
+    expect(screen.getByDisplayValue('storage-bucket-name')).toBeInTheDocument;
+    expect(screen.getByDisplayValue('storage-bucket-region')).toBeInTheDocument;
+    expect(screen.getByDisplayValue('storage-s3-url')).toBeInTheDocument;
+    expect(screen.getByDisplayValue('storage-s3-user')).toBeInTheDocument;
+    expect(screen.getByDisplayValue('storage-s3-password')).toBeInTheDocument;
+    expect(screen.getByRole('button', { name: 'Add Repository' })).toBeEnabled;
+  });
+});
