c532bad47a73dce3765d6df8e96653fc0fda51d8
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-07-06 14:31:19 -0400

    Rename start:remote:local-prod-test to start:remote:localprod, move files

--- scripts/local-prod-test/local-prod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export DEVMODE=remote
-export MIGMETA_FILE="$_dir/../../tmp/migmeta.json"
-export VIEWS_DIR="$_dir/../../public"
-export STATIC_DIR="$_dir/../../dist"
-yarn localbuild && node $_dir/../remote-dev.js && \
-  (cd $_dir && node ../../deploy/main.js)
-
