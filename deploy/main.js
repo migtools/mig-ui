04e850f5d12ffd56262b6b8396cbf4e9561cae13
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-07-13 13:12:17 -0400

    Normalize yarn start:remote and start:remote:localprod so dev servers use the same node/express code as real prod (#957)
    * Fix existing localprod express route match
    
    * Factor out dev-mode localConfig/migMeta into a helper
    
    * Remove unnecessary mig_meta module that just created an object
    
    * Localprod build: generate migmeta.json instead of index.dev.html (breaks localprod server for now)
    
    * Generate temporary migmeta.json file in tmp/ instead of dist/ so it is never directly statically served
    
    * Prettier
    
    * Switch local-prod script to use deploy/main.js instead of its own express server source
    
    * Rename start:remote:local-prod-test to start:remote:localprod, move files
    
    * Move migmeta.json generation from webpack into remote-dev.js and factor out script to start local express
    
    * Run express and webpack-dev-server concurrently on yarn start:remote
    
    * Add auto-reload of the express server in dev mode via nodemon
    
    Co-authored-by: Ian Bolton <ibolton@redhat.com>

--- deploy/main.js
+++ deploy/main.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 const migMetaFile = process.env['MIGMETA_FILE'] || '/srv/migmeta.json';
 const viewsDir = process.env['VIEWS_DIR'] || '/srv/views';
 const staticDir = process.env['STATIC_DIR'] || '/srv/static';
-const migMetaStr = fs.readFileSync(migMetaFile, 'utf8')
+const port = process.env['EXPRESS_PORT'] || 9000;
+
+const migMetaStr = fs.readFileSync(migMetaFile, 'utf8');
 
 console.log('migMetaFile: ', migMetaFile);
 console.log('viewsDir: ', viewsDir);
@@ -18,12 +20,18 @@ const app = express();
 app.engine('ejs', require('ejs').renderFile);
 app.set('views', viewsDir);
 app.use(express.static(staticDir));
-const port = 9000;
+
+// NOTE: Any future backend-only routes here need to also be proxied by webpack-dev-server.
+//       Add them to config/webpack.dev.js in the array under devServer.proxy.context.
+app.get('/hello', (req, res) => {
+  // NATODO remove this /hello example once we have some real routes here
+  res.send('Hello from Express!');
+});
 
 app.get('*', (req, res) => {
   res.render('index.ejs', { migMeta: encodedMigMeta });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
