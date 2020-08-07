821a4d18a45aeb1c3c5c31a566b987fd49e51991
Ian Bolton <ibolton@redhat.com>
2020-08-07 07:49:28 -0400

    Refactor plan status (#978)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * update status icons and text

---
+++ src/app/plan/duck/helpers.ts
@@ -0,0 +1,34 @@
+import { IPlan, IMigPlan, IMigration, ICondition, IStatus } from '../../plan/duck/types';
+interface IPlanConditionStatuses {
+  hasClosedCondition: boolean;
+  hasReadyCondition: boolean;
+  hasNotReadyCondition: boolean;
+  hasConflictCondition: boolean;
+  conflictErrorMsg: string;
+  hasPODWarnCondition: boolean;
+  hasPVWarnCondition: boolean;
+}
+
+interface ILatestMigrationConditionStatuses {
+  latestIsFailed: boolean;
+  hasCancelingCondition: boolean;
+  hasCanceledCondition: boolean;
+}
+
+export const filterPlanConditions = (conditions: ICondition[]): IPlanConditionStatuses => ({
+  hasClosedCondition: conditions.some((c) => c.type === 'Closed'),
+  hasReadyCondition: conditions.some((c) => c.type === 'Ready'),
+  hasNotReadyCondition: conditions.some((c) => c.category === 'Critical'),
+  hasConflictCondition: conditions.some((c) => c.type === 'PlanConflict'),
+  hasPODWarnCondition: conditions.some((c) => c.type === 'PodLimitExceeded'),
+  hasPVWarnCondition: conditions.some((c) => c.type === 'PVLimitExceeded'),
+  conflictErrorMsg: conditions.find((c) => c.type === 'PlanConflict')?.message,
+});
+
+export const filterLatestMigrationConditions = (
+  conditions: ICondition[]
+): ILatestMigrationConditionStatuses => ({
+  latestIsFailed: conditions.some((c) => c.type === 'Failed'),
+  hasCancelingCondition: conditions.some((c) => c.type === 'Canceling'),
+  hasCanceledCondition: conditions.some((c) => c.type === 'Canceled'),
+});
