7efc65bdced5c7aa9d617babbf8d0a3dd266cd1b
Ian Bolton <ibolton@redhat.com>
2020-08-17 17:19:03 -0400

    Update header for rebrand (#987)
    * Update header for rebrand
    
    * style cleanup
    
    * Update style for logo image
    
    * Add conditional logo height
    
    * remove span component type indicator
    
    Co-authored-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- src/app/common/components/PageHeaderComponent.tsx
+++ src/app/common/components/PageHeaderComponent.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { CogIcon, UserIcon } from '@patternfly/react-icons';
-import openshiftLogo from './CAM_LOGO.svg';
+import logoKonveyor from './logoKonveyor.svg';
+import logoRedHat from './logoRedHat.svg';
 import IconWithText from './IconWithText';
 import { IReduxState } from '../../../reducers';
 import {
@@ -16,9 +17,10 @@ import {
   ButtonVariant,
 } from '@patternfly/react-core';
 import accessibleStyles from '@patternfly/react-styles/css/utilities/Accessibility/accessibility';
-
 import { css } from '@patternfly/react-styles';
 import { NON_ADMIN_ENABLED } from '../../../TEMPORARY_GLOBAL_FLAGS';
+import { APP_TITLE } from '../../../app/common/constants';
+import { APP_BRAND, BrandType } from '../../global-flags';
 const styles = require('./PageHeaderComponent.module');
 
 interface PageHeaderComponentProps extends Omit<PageHeaderProps, 'logo'> {
@@ -59,11 +61,25 @@ const PageHeaderComponent: React.FunctionComponent<PageHeaderComponentProps> = (
             </ToolbarItem>
           )}
         </ToolbarGroup>
+        <ToolbarGroup>
+          <ToolbarItem>
+            <img
+              src={APP_BRAND === BrandType.RedHat ? logoRedHat : logoKonveyor}
+              alt="Logo"
+              className={
+                APP_BRAND === BrandType.RedHat ? styles.redhatLogoStyle : styles.konveyorLogoStyle
+              }
+            />
+          </ToolbarItem>
+        </ToolbarGroup>
       </Toolbar>
     }
     logo={
       <>
-        <Brand className={styles.logoPointer} src={openshiftLogo} alt="OpenShift Logo" />
+        <Title className={styles.logoPointer} headingLevel="h1" size="2xl">
+          {APP_TITLE}
+        </Title>
+
         {NON_ADMIN_ENABLED && (
           <Title
             size="2xl"
