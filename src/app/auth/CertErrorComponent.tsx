8d6f707faa37a54c853a8c6cbc1a5b1339a9a0c4
Erik Nelson <erik@nsk.io>
2020-10-27 12:36:45 -0400

    Cert error copy update (#1044)
    * Cert error copy update
    
    * Linter fix

--- src/app/auth/CertErrorComponent.tsx
+++ src/app/auth/CertErrorComponent.tsx
@@ -14,14 +14,14 @@ class CertErrorComponent extends React.Component<any> {
       <div>
         {this.props.certError ? (
           <div>
-            A certificate error has occurred, likely due to the usage of self signed certificates in
-            one of the clusters. Please try to visit the failed url and accept the CA:
+            A certificate error has occurred, likely caused by using self-signed CA certificates in
+            one of the clusters. Navigate to the following URL and accept the certificate:
             <a href={this.props.certError.failedUrl}> {this.props.certError.failedUrl}</a>
             <div />
-            The correct way to address this is to install your self CA into your browser.
+            If an "Unauthorized" message appears after you have accepted the certificate, refresh
+            the web page.
             <div />
-            NOTE: The contents of the resulting page may report "unauthorized". This is expected.
-            After accepting the certificate, please reload the app.
+            To fix this issue permanently, add the certificate to your web browser's trust store.
           </div>
         ) : (
           <div />
