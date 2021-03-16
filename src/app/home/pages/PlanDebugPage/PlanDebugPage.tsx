e8c08600253f02fd9642a527ef9df0e6ba3db4ca
Ian Bolton <ibolton@redhat.com>
2021-03-16 13:22:26 -0400

    upgrade pf and move debug view to expandable card

--- src/app/home/pages/PlanDebugPage/PlanDebugPage.tsx
+++ src/app/home/pages/PlanDebugPage/PlanDebugPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import spacing from '@patternfly/react-styles/css/utilities/Spacing/spacing';
 import {
   PageSection,
   Bullseye,
@@ -16,6 +17,11 @@ import {
   SplitItem,
   SearchInput,
   Button,
+  CardHeader,
+  CardExpandableContent,
+  CardFooter,
+  Popover,
+  PopoverPosition,
 } from '@patternfly/react-core';
 import { push } from 'connected-react-router';
 import {
@@ -26,11 +32,18 @@ import {
 
 import { convertRawTreeToViewTree } from '../../../debug/duck/utils';
 import { treeFetchRequest } from '../../../debug/duck/slice';
+import { ExclamationCircleIcon } from '@patternfly/react-icons/dist/js/icons/exclamation-circle-icon';
+import {
+  ExclamationTriangleIcon,
+  QuestionCircleIcon,
+  SpaIconConfig,
+} from '@patternfly/react-icons/dist/js/icons';
 
 export const PlanDebugPage: React.FunctionComponent = () => {
   const { planName } = useParams();
   const dispatch = useDispatch();
   const debug = useSelector((state) => state.debug);
+  const [isOpen, setIsOpen] = useState(false);
 
   const refreshDebugTree = () => {
     dispatch(treeFetchRequest(planName));
@@ -79,11 +92,6 @@ export const PlanDebugPage: React.FunctionComponent = () => {
 
   return (
     <>
-      <PageSection variant="light">
-        <Title headingLevel="h1" size="2xl">
-          Migration plan resources (DEBUG)
-        </Title>
-      </PageSection>
       <PageSection>
         {debug.errMsg ? (
           <Alert variant="danger" title={`Error loading debug data for plan "${planName}"`}>
@@ -101,25 +109,64 @@ export const PlanDebugPage: React.FunctionComponent = () => {
             </EmptyState>
           </Bullseye>
         ) : (
-          <Card>
-            <CardBody>
-              <Split hasGutter>
-                <SplitItem isFilled>
-                  <SearchInput
-                    placeholder="Type to search"
-                    value={searchText}
-                    onChange={setSearchText}
-                    onClear={() => setSearchText('')}
-                  />
-                </SplitItem>
-                <SplitItem>
-                  <Button onClick={refreshDebugTree} variant="primary">
-                    Refresh
-                  </Button>
-                </SplitItem>
-              </Split>
-              <TreeView data={filteredTreeData} defaultAllExpanded />
-            </CardBody>
+          <Card id="image-card" isExpanded={isOpen}>
+            <CardHeader
+              onExpand={() => setIsOpen(!isOpen)}
+              toggleButtonProps={{
+                id: 'toggle-button',
+                'aria-label': 'debug-details',
+                'aria-expanded': isOpen,
+              }}
+            >
+              <Title headingLevel="h1" size="xl" className={spacing.mrLg}>
+                Migration plan resources
+              </Title>
+              <Popover
+                position={PopoverPosition.bottom}
+                bodyContent={
+                  <>
+                    <Title headingLevel="h2" size="xl">
+                      <>Debug view</>
+                    </Title>
+                    <p className={spacing.mtMd}>
+                      View the Kubernetes resources created by the migration process. Currently
+                      active resources are highlighted. This view is helpful for debugging migration
+                      issues.
+                    </p>
+                  </>
+                }
+                aria-label="operator-mismatch-details"
+                closeBtnAriaLabel="close--details"
+                maxWidth="30rem"
+              >
+                <span>
+                  <span className="pf-c-icon pf-m-info">
+                    <QuestionCircleIcon size="md" />
+                  </span>
+                </span>
+              </Popover>
+            </CardHeader>
+            <CardExpandableContent>
+              <CardBody>
+                <Split hasGutter>
+                  <SplitItem isFilled>
+                    <SearchInput
+                      placeholder="Type to search"
+                      value={searchText}
+                      onChange={setSearchText}
+                      onClear={() => setSearchText('')}
+                    />
+                  </SplitItem>
+                  <SplitItem>
+                    <Button onClick={refreshDebugTree} variant="primary">
+                      Refresh
+                    </Button>
+                  </SplitItem>
+                </Split>
+                <TreeView data={filteredTreeData} defaultAllExpanded />
+              </CardBody>
+              {/* <CardFooter>Footer</CardFooter> */}
+            </CardExpandableContent>
           </Card>
         )}
       </PageSection>
