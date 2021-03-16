dcc787fe0b311bc03935a4101d75eb0d5998249e
Ian Bolton <ibolton@redhat.com>
2021-03-16 11:53:08 -0400

    move drill down components to a nested route

--- src/app/home/pages/PlansPage/pages/MigrationsPage/MigrationsPage.tsx
+++ src/app/home/pages/PlansPage/pages/MigrationsPage/MigrationsPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useParams, Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import spacing from '@patternfly/react-styles/css/utilities/Spacing/spacing';
+import { useParams, useRouteMatch, Link, Switch, Route, Redirect } from 'react-router-dom';
 import {
   PageSection,
   Title,
@@ -13,42 +13,55 @@ import {
 import { IPlan } from '../../../../../plan/duck/types';
 import { planSelectors } from '../../../../../plan/duck';
 import MigrationsTable from '../../components/MigrationsTable';
+import { MigrationStepDetailsPage } from '../MigrationStepDetailsPage';
+import { MigrationDetailsPage } from '../MigrationDetailsPage';
 
 export const MigrationsPage: React.FunctionComponent = () => {
   const { planName } = useParams();
   const planList = useSelector((state) => planSelectors.getPlansWithStatus(state));
   const plan = planList.find((planItem: IPlan) => planItem.MigPlan.metadata.name === planName);
+  const { path, url } = useRouteMatch();
 
   return (
     <>
-      <PageSection variant="light">
-        <Breadcrumb className={`${spacing.mbLg} ${spacing.prLg}`}>
-          <BreadcrumbItem>
-            <Link to="/plans">Plans</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem to="#" isActive>
-            {planName}
-          </BreadcrumbItem>
-        </Breadcrumb>
-        <Title headingLevel="h1" size="2xl">
-          Migrations
-        </Title>
-      </PageSection>
-      {!plan ? null : (
-        <PageSection>
-          <Card>
-            <CardBody>
-              <MigrationsTable
-                type="Migrations"
-                planName={planName}
-                migrations={plan.Migrations}
-                isPlanLocked={plan.PlanStatus.isPlanLocked}
-                id="migrations-history-expansion-table"
-              />
-            </CardBody>
-          </Card>
-        </PageSection>
-      )}
+      <Switch>
+        <Route exact path={path}>
+          <PageSection variant="light">
+            <Breadcrumb className={`${spacing.mbLg} ${spacing.prLg}`}>
+              <BreadcrumbItem>
+                <Link to="/plans">Plans</Link>
+              </BreadcrumbItem>
+              <BreadcrumbItem to="#" isActive>
+                {planName}
+              </BreadcrumbItem>
+            </Breadcrumb>
+            <Title headingLevel="h1" size="2xl">
+              Migrations
+            </Title>
+          </PageSection>
+          {!plan ? null : (
+            <PageSection>
+              <Card>
+                <CardBody>
+                  <MigrationsTable
+                    type="Migrations"
+                    planName={planName}
+                    migrations={plan.Migrations}
+                    isPlanLocked={plan.PlanStatus.isPlanLocked}
+                    id="migrations-history-expansion-table"
+                  />
+                </CardBody>
+              </Card>
+            </PageSection>
+          )}
+        </Route>
+        <Route path={`${path}/:migrationID`}>
+          <MigrationDetailsPage />
+        </Route>
+        <Route path={`${path}/:migrationID/:stepName`}>
+          <MigrationStepDetailsPage />
+        </Route>
+      </Switch>
     </>
   );
 };
