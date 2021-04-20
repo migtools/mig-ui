e746b63381526a35ae6a1ac2e0f3824efd61dd4e
Ian Bolton <ibolton@redhat.com>
2021-04-19 20:55:11 -0400

    wip

--- src/app/home/pages/PlanDebugPage/components/TestTree.tsx
+++ src/app/home/pages/PlanDebugPage/components/TestTree.tsx
@@ -1,83 +1,17 @@
 import React, { useState } from 'react';
 import { Table, TableHeader, TableBody, headerCol, treeRow } from '@patternfly/react-table';
 import { table } from 'console';
+interface ITreeTableProps {
+  filteredTreeData: any;
+}
 
-export const TreeTable: React.FunctionComponent = (props: any) => {
-  const { filteredTreeData } = props;
+export const TreeTable: React.FunctionComponent<ITreeTableProps> = ({ filteredTreeData }) => {
   //   buildRows: ([x, ...xs]: [any, ...any[]], level: any, posinset: any, isHidden?: boolean) => any[];
   //   onCollapse: (event: any, rowIndex: any, title: any) => void;
   //   onCheckChange: (event: any, checked: any, rowIndex: any, title: any) => void;
   //   state: any;
   //   prevState: any;
   const [expandedRows, setExpandedRows] = useState([]);
-  const [tableTreeData, setTableTreeData] = useState([
-    {
-      repositories: 'Repositories one',
-      branches: <span> testing this </span>,
-      pullRequests: 'Pull request one',
-      workspaces: 'Workplace one',
-      children: [
-        {
-          repositories: 'Repositories two',
-          branches: 'Branch two',
-          pullRequests: 'Pull request two',
-          workspaces: 'Workplace two',
-          children: [
-            {
-              repositories: 'Repositories three',
-              branches: 'Branch three',
-              pullRequests: 'Pull request three',
-              workspaces: 'Workplace three',
-            },
-          ],
-        },
-        {
-          repositories: 'Repositories four',
-          branches: 'Branch four',
-          pullRequests: 'Pull request four',
-          workspaces: 'Workplace four',
-        },
-        {
-          repositories: 'Repositories five',
-          branches: 'Branch five',
-          pullRequests: 'Pull request five',
-          workspaces: 'Workplace five',
-        },
-      ],
-    },
-    {
-      repositories: 'Repositories six',
-      branches: 'Branch six',
-      pullRequests: 'Pull request six',
-      workspaces: 'Workplace six',
-      children: [
-        {
-          repositories: 'Repositories seven',
-          branches: 'Branch seven',
-          pullRequests: 'Pull request seven',
-          workspaces: 'Workplace seven',
-        },
-      ],
-    },
-    {
-      repositories: 'Repositories eight',
-      branches: 'Branch eight',
-      pullRequests: 'Pull request eight',
-      workspaces: 'Workplace eight',
-    },
-  ]);
-  //   expandedRows: ['Repositories one', 'Repositories six'],
-  //   checkedRows: [],
-
-  /** 
-      Recursive function which flattens the data into an array flattened IRow objects 
-      to be passed to the `rows` prop of the Table
-      params: 
-        - rowData - array of data
-        - level - number representing how deeply nested the current row is
-        - posinset - position of the row relative to this row's siblings
-        - isHidden - defaults to false, true if this row's parent is expanded
-    */
   const buildRows = ([x, ...xs], level, posinset, isHidden = false) => {
     if (x) {
       const isExpanded = expandedRows.includes(x.id);
@@ -117,7 +51,6 @@ export const TreeTable: React.FunctionComponent = (props: any) => {
       isTreeTable
       aria-label="Tree table"
       cells={[{ title: 'ID', cellTransforms: [treeRow(onCollapse)] }, 'Name']}
-      //@ts-ignore
       rows={buildRows(filteredTreeData, 1, 1)}
     >
       <TableHeader />
