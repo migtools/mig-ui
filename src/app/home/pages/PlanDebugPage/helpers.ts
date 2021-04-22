ecaf6a3d18e0e0e61a0e3aa65b9928d7bca0fbae
Derek Whatley <derekwhatley@gmail.com>
2021-04-22 12:17:19 -0400

    Fix oc get commands

--- src/app/home/pages/PlanDebugPage/helpers.ts
+++ src/app/home/pages/PlanDebugPage/helpers.ts
@@ -14,10 +14,26 @@ export const getFullKindName = (kind: any) => {
       return 'podvolumerestore';
     case 'Restore':
       return 'restore';
-    case 'DirectVolume':
+    case 'DirectVolumeMigration':
       return 'directvolumemigration';
-    case 'DirectImage':
+    case 'DirectImageMigration':
       return 'directimagemigration';
+    case 'DirectImageStreamMigration':
+      return 'directimagestreammigration';
+    case 'DirectVolumeMigrationProgress':
+      return 'directvolumemigrationprogress';
+    case 'Hook':
+      return 'mighook';
+    case 'Job':
+      return 'job';
+    case 'Pod':
+      return 'pod';
+    case 'PV':
+      return 'persistentvolume';
+    case 'PVC':
+      return 'persistentvolumeclaim';
+    case 'Route':
+      return 'route';
     default:
       return kind;
   }
@@ -29,6 +45,46 @@ interface ICommandAndTypeObj {
 export const getOCCommandAndClusterType = (rawNode: IDebugTreeNode): ICommandAndTypeObj => {
   const { kind, namespace, name } = rawNode;
   switch (kind) {
+    case 'DirectImageStreamMigration':
+      return {
+        ocCommand: `oc get ${getFullKindName(kind)} -n ${namespace} ${name}`,
+        clusterType: '',
+      };
+    case 'DirectVolumeMigrationProgress':
+      return {
+        ocCommand: `oc get ${getFullKindName(kind)} -n ${namespace} ${name}`,
+        clusterType: '',
+      };
+    case 'Hook':
+      return {
+        ocCommand: `oc get ${getFullKindName(kind)} -n ${namespace} ${name}`,
+        clusterType: '',
+      };
+    case 'Job':
+      return {
+        ocCommand: `oc get ${getFullKindName(kind)} -n ${namespace} ${name}`,
+        clusterType: '',
+      };
+    case 'PV':
+      return {
+        ocCommand: `oc get ${getFullKindName(kind)} ${name}`,
+        clusterType: '',
+      };
+    case 'PVC':
+      return {
+        ocCommand: `oc get ${getFullKindName(kind)} -n ${namespace} ${name}`,
+        clusterType: '',
+      };
+    case 'Route':
+      return {
+        ocCommand: `oc get ${getFullKindName(kind)} -n ${namespace} ${name}`,
+        clusterType: '',
+      };
+    case 'Pod':
+      return {
+        ocCommand: `oc get ${getFullKindName(kind)} -n ${namespace} ${name}`,
+        clusterType: '',
+      };
     case 'Migration':
       return {
         ocCommand: `oc get ${getFullKindName(kind)} -n ${namespace} ${name}`,
@@ -59,12 +115,12 @@ export const getOCCommandAndClusterType = (rawNode: IDebugTreeNode): ICommandAnd
         ocCommand: `oc get ${getFullKindName(kind)} -n ${namespace} ${name}`,
         clusterType: 'target',
       };
-    case 'DirectVolume':
+    case 'DirectVolumeMigration':
       return {
         ocCommand: `oc get ${getFullKindName(kind)} -n ${namespace} ${name}`,
         clusterType: '',
       };
-    case 'DirectImage':
+    case 'DirectImageMigration':
       return {
         ocCommand: `oc get ${getFullKindName(kind)} -n ${namespace} ${name}`,
         clusterType: '',
