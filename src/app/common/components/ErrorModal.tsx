e2b8bab19f44a5a1faee17d1a588dec5b9de3c25
Ian Bolton <ibolton@redhat.com>
2021-05-10 12:43:57 -0400

    update

--- src/app/common/components/ErrorModal.tsx
+++ src/app/common/components/ErrorModal.tsx
@@ -26,7 +26,7 @@ const ErrorModal: React.FunctionComponent<IProps> = (props) => {
     <React.Fragment>
       <Title headingLevel={'h1'} size={BaseSizes['2xl']}>
         <ErrorCircleOIcon className={styles.errorIconStyles} />
-        {`Error while fetching ${errorModalObj?.name || 'data'}`}
+        {`Error`}
       </Title>
     </React.Fragment>
   );
@@ -39,7 +39,7 @@ const ErrorModal: React.FunctionComponent<IProps> = (props) => {
       onClose={() => {
         dispatch(errorModalClear());
       }}
-      title={`Error while fetching ${errorModalObj.name || 'data'}`}
+      title={`Error'}`}
     >
       <Grid hasGutter>
         <form>
@@ -57,21 +57,37 @@ const ErrorModal: React.FunctionComponent<IProps> = (props) => {
             <>
               <br />
               <div>
-                A certificate error has occurred, likely caused by using self-signed CA certificates
-                in one of the clusters. Navigate to the following URL and accept the certificate:
+                A network error has occurred and we are unable to fetch one or more resources. We
+                are unable to determine the precise cause due to browser obfuscation, but based on
+                the content of the error it is likely one of two root issues:
                 <br />
                 <br />
-                <a target="_blank" href={certError.failedUrl}>
-                  {certError.failedUrl}
-                </a>
-                <br />
-                <br />
-                <div />
-                If an "Unauthorized" message appears after you have accepted the certificate,
-                navigate to the MTC console and refresh the page.
-                <div />
-                To fix this issue permanently, add the certificate to your web browser's trust
-                store.
+                <ul>
+                  <li>
+                    * Your cluster API and/or its services are using self-signed certificates and
+                    you have not installed the CA as trusted. You can grant an exception within your
+                    browser by visiting the following link and accepting the certificate:
+                    <br />
+                    <br />
+                    <a target="_blank" href={certError.failedUrl}>
+                      {certError.failedUrl}
+                    </a>
+                    <br />
+                    <br />
+                    NOTE: It is recommended if using a self-signed certificate to install the
+                    trusted CA in your browser’s supported store prior to using the Migration
+                    Toolkit.
+                  </li>
+                  <br />
+                  <br />
+                  <li>
+                    * If you are sure that you have already accepted the requisite CAs, it could be
+                    a Cross Origin Resource Sharing rejection. This is something that is configured
+                    in the cluster’s API server settings by the MTC operator and can take up to 10
+                    minutes to propagate across all related services. Please wait and check back in
+                    at a later point in time.
+                  </li>
+                </ul>
               </div>
             </>
           )}
