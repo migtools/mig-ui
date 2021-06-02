e44aa207c82a7c39ab1b4e756b38f8187ad968c0
Ian Bolton <ibolton@redhat.com>
2021-06-02 12:56:58 -0400

    wip

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -1,12 +1,17 @@
 import { history } from './helpers';
 import { createLogger } from 'redux-logger';
+import { configureStore } from '@reduxjs/toolkit';
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import { routerMiddleware } from 'connected-react-router';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import createSagaMiddleware from 'redux-saga';
+import { getDefaultMiddleware } from '@reduxjs/toolkit';
+
 import rootSaga from './sagas';
+import commonReducer from './app/common/duck';
+import authReducer from './app/auth/duck';
 declare global {
   interface Window {
     __REDUX_DEVTOOLS_EXTENSION__: any;
@@ -15,20 +20,21 @@ declare global {
 
 const devMode = process.env.DEVMODE || 'local';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  //whitelist: ['cluster', 'storage', 'plan'],
-  whitelist: [],
-};
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   //whitelist: ['cluster', 'storage', 'plan'],
+//   whitelist: [],
+// };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const sagaMiddleware = createSagaMiddleware();
 
 const enhancers = [];
 const logger = createLogger({ collapsed: true });
-const middleware = [logger, sagaMiddleware, routerMiddleware(history)];
+// const middleware = [logger, sagaMiddleware, routerMiddleware(history)];
+const middleware = [logger, sagaMiddleware];
 
 if (devMode === 'local') {
   const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
@@ -37,11 +43,23 @@ if (devMode === 'local') {
     enhancers.push(devToolsExtension());
   }
 }
-const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
+// const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
+//   const creadedStore = createStore<any, any, any, any>( composedEnhancers);
 
-const createdStore = () => {
-  const store = createStore<any, any, any, any>(persistedReducer, composedEnhancers);
-  sagaMiddleware.run(rootSaga);
-  return { store, persistor: persistStore(store) };
-};
-export default createdStore;
+// const createdStore = () => {
+//   sagaMiddleware.run(rootSaga);
+//   return { store, persistor: persistStore(store) };
+// };
+// ...
+const middlewareApplied = [middleware, ...getDefaultMiddleware()] as const;
+const store = configureStore({
+  rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: false,
+    }).concat(logger),
+  devTools: process.env.NODE_ENV !== 'production',
+  preloadedState,
+  enhancers: [reduxBatch],
+});
+export default store;
--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,30 @@ import { Provider } from 'react-redux';
 import { history } from './helpers';
 import AppRoutes from './routes';
 import { ConnectedRouter } from 'connected-react-router';
-import { PersistGate } from 'redux-persist/es/integration/react';
-import configureStore from './configureStore';
-const { persistor, store } = configureStore();
+// import { PersistGate } from 'redux-persist/es/integration/react';
+// import configureStore from './configureStore';
+import { configureStore } from '@reduxjs/toolkit';
+
+// const { persistor, store } = configureStore();
+// const store = configureStore({
+//   reducer: {
+//     s: postsReducer,
+//     comments: commentsReducer,
+//     users: usersReducer,
+//   },
+// });
 import { Spinner } from '@patternfly/react-core';
 import { AppLayout } from './layout/AppLayout';
 
 render(
   <Provider store={store}>
-    <PersistGate loading={<Spinner size="xl" />} persistor={persistor}>
-      <ConnectedRouter history={history}>
-        <AppLayout>
-          <AppRoutes />
-        </AppLayout>
-      </ConnectedRouter>
-    </PersistGate>
+    {/* <PersistGate loading={<Spinner size="xl" />} persistor={persistor}> */}
+    <ConnectedRouter history={history}>
+      <AppLayout>
+        <AppRoutes />
+      </AppLayout>
+    </ConnectedRouter>
+    {/* </PersistGate> */}
   </Provider>,
   document.getElementById('root')
 );
