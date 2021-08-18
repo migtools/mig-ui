eda85fb4c077931c0c14c39c4149f9e13b73ac00
Ian Bolton <ibolton@redhat.com>
2021-08-18 10:57:56 -0400

    disable migrate if all pvs skipped

--- src/app/common/helpers.ts
+++ src/app/common/helpers.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { INameNamespaceRef } from './duck/types';
 
 interface INameNamespaceRefSuperset extends INameNamespaceRef {
@@ -11,4 +12,38 @@ export const isSameResource = (
 
 export const validatedState = (touchedFieldVal: any, errorsFieldVal: any) => {
   return !(touchedFieldVal && errorsFieldVal) ? 'default' : 'error';
+};
+interface IDelayedQueryObject {
+  name: string;
+  row: any;
+  fieldName: string;
+}
+
+export const useDelayValidation = (delayedFunction: any) => {
+  const [query, setQuery] = useState<IDelayedQueryObject>({ name: '', row: null, fieldName: null });
+
+  useEffect(() => {
+    const timeOutId = setTimeout(
+      () =>
+        delayedFunction(query.fieldName, {
+          name: query.name,
+          srcName: query.row.cells[0],
+        }),
+      500
+    );
+    return () => clearTimeout(timeOutId);
+  }, [query]);
+
+  return { setQuery, query };
+
+  // return (
+  //   <>
+  //     <input
+  //       type="text"
+  //       value={query}
+  //       onChange={event => setQuery(event.target.value)}
+  //     />
+  //     <p>{displayMessage}</p>
+  //   </>
+  // );
 };
