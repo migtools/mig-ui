8af35b1cb226bedc16efcbbcba759814b7c86976
Ian Bolton <ibolton@redhat.com>
2021-08-25 12:55:40 -0400

    remove same cluster validation (#1288)

--- src/app/home/pages/PlansPage/components/Wizard/WizardFormik.tsx
+++ src/app/home/pages/PlansPage/components/Wizard/WizardFormik.tsx
@@ -38,18 +38,12 @@ const WizardFormik: React.FunctionComponent<IWizardFormikProps> = ({
 
       if (!values.sourceCluster) {
         errors.sourceCluster = 'Required';
-      } else if (values.sourceCluster === values.targetCluster) {
-        errors.sourceCluster =
-          'The selected source cluster must be different than the target cluster.';
       }
       if (!values.selectedNamespaces || values.selectedNamespaces.length === 0) {
         errors.selectedNamespaces = 'Required';
       }
       if (!values.targetCluster) {
         errors.targetCluster = 'Required';
-      } else if (values.sourceCluster === values.targetCluster) {
-        errors.targetCluster =
-          'The selected target cluster must be different than the source cluster.';
       }
       if (!values.selectedStorage) {
         errors.selectedStorage = 'Required';
